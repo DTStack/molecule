00700dfef50eb2290749b8488163240ef6585e4d
xiaowei <xiaowei@dtstack.com>
2020-10-27 10:24:24 +0800

    test: rename workbench.test.ts

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+export * from './activityBarService';
+export * from './eventService';
+export * from './extensionService';
+export * from './menuBarService';
+export * from './sidebarService';
+export * from './themeService';
+export * from './moleculeService';
+export * from './editor/editorService';
+
---
+++ src/services/themeService.ts
@@ -0,0 +1,70 @@
+/**
+ * VSCode theme extends guides: https://code.visualstudio.com/api/extension-guides/color-theme
+ * https://code.visualstudio.com/api/references/theme-color
+ */
+
+import { IExtension } from 'mo/core/extension';
+import { ITheme, ThemeColor, TokenColor } from 'mo/core/theme';
+import { singleton } from 'tsyringe';
+import * as monaco from 'monaco-editor';
+
+/**
+ * Apply css content to workbench
+ * @param styleSheetContent CSS sheet content
+ * @param rulesClassName Style tag class Name
+ */
+function _applyRules(styleSheetContent: string, rulesClassName: string) {
+    const themeStyles = document.head.getElementsByClassName(rulesClassName);
+    if (themeStyles.length === 0) {
+        const elStyle = document.createElement('style');
+        elStyle.type = 'text/css';
+        elStyle.className = rulesClassName;
+        elStyle.innerHTML = styleSheetContent;
+        document.head.appendChild(elStyle);
+    } else {
+        (<HTMLStyleElement>themeStyles[0]).innerHTML = styleSheetContent;
+    }
+}
+
+@singleton()
+export class ThemeService implements ITheme {
+    id: string;
+    name: string;
+    colors: ThemeColor;
+    tokenColors: TokenColor[];
+    semanticHighlighting: boolean;
+
+    constructor(
+        id: string,
+        name: string,
+        colors: ThemeColor = [],
+        tokenColors: TokenColor[] = [],
+        semanticHighlighting: boolean = true,
+    ) {
+        this.id = id;
+        this.name = name;
+        this.colors = colors;
+        this.tokenColors = tokenColors;
+        this.semanticHighlighting = semanticHighlighting;
+        this.init();
+    }
+
+    public init() {
+        monaco.editor.setTheme(this.id);
+    }
+
+    public load(extension: IExtension): void {
+        // const theme = this.parse(extension);
+    }
+
+    public getThemeById(themeId: string, extension: IExtension) {
+
+    }
+
+    public applyTheme() {
+        _applyRules('', '');
+    }
+}
+
+export const themeService = new ThemeService('vs-dark', 'vs-dark');
+

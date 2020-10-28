550ada9b44063198764747340a69df79971e7341
xiaowei <xiaowei@dtstack.com>
2020-10-28 10:40:57 +0800

    refactor: simplify default exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import 'reflect-metadata';
-import 'mo/style/main.scss';
-import 'vscode-codicons/dist/codicon.css';
-
-export * from 'mo/workbench';
-export * from 'mo/common/event';
-export * from 'mo/core/workbench/activityBar';
 export * from 'mo/services';
 export * from 'mo/provider';
-
+export * from 'mo/workbench';
+export * from 'mo/common/event';
+export * from 'mo/core';

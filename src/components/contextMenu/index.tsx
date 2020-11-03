606f73cb32a2291f76b802b2f8b2e752e368f442
xiaowei <xiaowei@dtstack.com>
2020-11-03 17:05:11 +0800

    feat: add contextMenu component

---
+++ src/components/contextMenu/index.tsx
@@ -0,0 +1,24 @@
+// import * as React from 'react';
+// import { prefixClaName } from 'mo/common/className';
+// import { ContextView } from 'monaco-editor/esm/vs/base/browser/ui/contextview/contextview';
+// import { StandardMouseEvent } from 'monaco-editor/esm/vs/base/browser/mouseEvent';
+
+// export interface IContextMenuProps {
+
+// }
+
+// const ContextMenu: React.FunctionComponent = function(props) {
+//     const view = new ContextView();
+
+//     return (
+//         <div className={`${prefixClaName('contextmenu')}`} >
+//             {
+//                 props.children
+//             }
+//         </div>
+//     );
+// }
+
+// export {
+//     ContextMenu
+// }

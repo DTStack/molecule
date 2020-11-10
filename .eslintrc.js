e86085ca401abac1fff90706fe6234c3ebafdfc8
xiaowei <xiaowei@dtstack.com>
2020-11-10 19:03:59 +0800

    build: add code prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
         browser: true,
         es6: true,
     },
-    extends: ['plugin:react/recommended', 'google', 'prettier', 'prettier/react'],
+    extends: [
+        'plugin:react/recommended',
+        'google',
+        'prettier',
+        'prettier/react',
+    ],
     globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',

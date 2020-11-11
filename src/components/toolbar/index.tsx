0c35c3be31b893f06f065899b266b647f67faf2b
xiaowei <xiaowei@dtstack.com>
2020-11-11 21:44:24 +0800

    feat: add ToolBar component

---
+++ src/components/toolbar/index.tsx
@@ -0,0 +1,19 @@
+import './style.scss';
+import * as React from 'react';
+import { prefixClaName, classNames } from 'mo/common/className';
+import ActionBar, { IActionBar } from 'mo/components/actionbar';
+
+export interface IToolBar<T = any> extends IActionBar {
+}
+
+const rootClassName = 'tool-bar';
+
+export default function ToolBar<T = any>(props: IToolBar<T>) {
+    const { className, ...others } = props;
+
+    return (
+        <div className={classNames(prefixClaName(rootClassName), className)}>
+            <ActionBar {...others}/>
+        </div>
+    )
+}

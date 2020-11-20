c9e291e12b47650dd7ac082c016f1a4a1e995054
xiaowei <xiaowei@dtstack.com>
2020-11-20 14:33:40 +0800

    feat: add em2Px function

---
+++ src/common/css.ts
@@ -0,0 +1,9 @@
+
+/**
+ * px = em * parentElementFontSize
+ * @param em em value
+ * TODO: Use Template Literal Types replace fontSize typing
+ */
+export function em2Px(em: number, fontSize: number): number {
+    return em * fontSize;
+}

3c38959b9d9081e6076debe73af86ac55d562af2
xiaowei <xiaowei@dtstack.com>
2020-11-25 17:25:46 +0800

    feat(react): add cloneReactChildren function

---
+++ src/react/helper.ts
@@ -0,0 +1,15 @@
+import { Children, cloneElement, isValidElement } from "react";
+
+/**
+ * Clone react children props
+ * @param children ReactNode
+ * @param props Parent props
+ */
+export function cloneReactChildren<P>(children: React.ReactNode, props: P): ReactNode {
+    return Children.map(children, child => {
+        if (isValidElement(child)) {
+            return cloneElement(child, props);
+        }
+        return child;
+    })
+}
--- src/react/index.ts
+++ src/react/index.ts
@@ -1,2 +1,3 @@
 export * from './component';
 export * from './mapState';
+export * from './helper';

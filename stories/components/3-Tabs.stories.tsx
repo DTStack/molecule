78c25f9ea38495fa779468282d6ec909ace9b16a
木杪 <mumiao@dtstack.com>
2020-11-25 20:39:45 +0800

    feat: add draft tabs stories

--- stories/components/3-Tabs.stories.tsx
+++ stories/components/3-Tabs.stories.tsx
@@ -1,15 +1,49 @@
 import * as React from 'react';
+import { useState } from 'react';
+import Tabs from 'mo/components/tabs';
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+const stories = storiesOf('Tab', module);
+stories.addDecorator(withKnobs);
 
-import Tabs, { ITab } from 'mo/components/tabs';
-
-export default {
-    title: 'Tabs',
-};
-
-const data: ITab[] = [
-    {
-        id: 1,
-        name: 'test',
-    },
-];
-export const Basic = () => <Tabs data={data} />;
+stories.add('Basic Usage', () => {
+    const onMoveTab = (tabs) => setTabs(tabs);
+    const data = [
+        {
+            modified: true,
+            id: 1,
+            name: 'editor.js',
+            value: 'hello javascript',
+        },
+        {
+            activeTab: 1,
+            modified: false,
+            id: 2,
+            name: 'editor.css',
+            value: 'hello css',
+        },
+        {
+            modified: false,
+            id: 3,
+            name: 'editor.tsx',
+            value: 'hello tsx',
+        },
+    ];
+    const [tabs, setTabs] = useState(data);
+    return (
+        <div>
+            <h2>简述</h2>
+            <p>Tab 提供组件多tab切换；拖拽</p>
+            <div>
+                <h3>使用示例 1 - 基本使用</h3>
+                <div style={{ height: 200 }}>
+                    <Tabs
+                        data={tabs}
+                        onMoveTab={onMoveTab}
+                        onTabChange={(data) => console.log(data)}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+});

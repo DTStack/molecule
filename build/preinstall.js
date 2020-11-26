44c11d3670bd46d71b927b807c5e89c163e53f59
xiaowei <xiaowei@dtstack.com>
2020-11-26 13:37:25 +0800

    style: prettify code

--- build/preinstall.js
+++ build/preinstall.js
@@ -1,31 +1,31 @@
-let err = false
+let err = false;
 
-const majorNodeVersion = parseInt(/^(\d+)\./.exec(process.versions.node)[1])
+const majorNodeVersion = parseInt(/^(\d+)\./.exec(process.versions.node)[1]);
 
 if (majorNodeVersion < 10 || majorNodeVersion > 14) {
-    console.error('\033[1;31m*** Please use node >=10 and <=14.\033[0;0m')
-    err = true
+    console.error('\033[1;31m*** Please use node >=10 and <=14.\033[0;0m');
+    err = true;
 }
 
-const cp = require('child_process')
-const yarnVersion = cp.execSync('yarn -v', { encoding: 'utf8' }).trim()
-const parsedYarnVersion = /^(\d+)\.(\d+)\./.exec(yarnVersion)
-const majorYarnVersion = parseInt(parsedYarnVersion[1])
-const minorYarnVersion = parseInt(parsedYarnVersion[2])
+const cp = require('child_process');
+const yarnVersion = cp.execSync('yarn -v', { encoding: 'utf8' }).trim();
+const parsedYarnVersion = /^(\d+)\.(\d+)\./.exec(yarnVersion);
+const majorYarnVersion = parseInt(parsedYarnVersion[1]);
+const minorYarnVersion = parseInt(parsedYarnVersion[2]);
 
 if (majorYarnVersion < 1 || minorYarnVersion < 10) {
-    console.error('\033[1;31m*** Please use yarn >=1.10.1.\033[0;0m')
-    err = true
+    console.error('\033[1;31m*** Please use yarn >=1.10.1.\033[0;0m');
+    err = true;
 }
 
 if (!/yarn[\w-.]*\.js$|yarnpkg$/.test(process.env['npm_execpath'])) {
     console.error(
         '\033[1;31m*** Please use yarn to install dependencies.\033[0;0m'
-    )
-    err = true
+    );
+    err = true;
 }
 
 if (err) {
-    console.error('')
-    process.exit(1)
+    console.error('');
+    process.exit(1);
 }

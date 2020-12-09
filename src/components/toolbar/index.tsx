c54d2897617de6997aeeee67981d1e318e11c9e2
xiaowei <xiaowei@dtstack.com>
2020-12-09 16:40:22 +0800

    refactor(toolbar): convert the naming to BEM standard

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -3,16 +3,16 @@ import * as React from 'react';
 import { prefixClaName, classNames } from 'mo/common/className';
 import ActionBar, { IActionBar } from 'mo/components/actionbar';
 
-export interface IToolBar<T = any> extends IActionBar {}
+export interface IToolBar<T = any> extends IActionBar { }
 
 const rootClassName = 'tool-bar';
 
 export default function ToolBar<T = any>(props: IToolBar<T>) {
-    const { className, ...others } = props;
+    const { className, ...custom } = props;
 
     return (
         <div className={classNames(prefixClaName(rootClassName), className)}>
-            <ActionBar {...others} />
+            <ActionBar {...custom} />
         </div>
     );
 }

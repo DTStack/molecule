e9acc20fd9bb557389e9ff9168fbd11abecdf8d4
xiaowei <xiaowei@dtstack.com>
2020-12-09 19:03:57 +0800

    refactor(panel): convert the naming to BEM standard

--- src/workbench/panel/index.tsx
+++ src/workbench/panel/index.tsx
@@ -3,10 +3,12 @@ import * as React from 'react';
 import { memo } from 'react';
 import { prefixClaName } from 'mo/common/className';
 
+const defaultClassName = prefixClaName('panel');
+
 function Panel(props) {
     console.log('Panel render:', props);
 
-    return <div className={prefixClaName('panel')}>Panel</div>;
+    return <div className={defaultClassName}>Panel</div>;
 }
 
 export default memo(Panel);

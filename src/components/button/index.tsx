19675d82ac7b0e8b2195477fc750e5a811697d43
xiaowei <xiaowei@dtstack.com>
2020-12-09 16:46:06 +0800

    refactor(button): convert the naming to BEM standard

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,6 +1,6 @@
 import './style.scss';
 import * as React from 'react';
-import { classNames, prefixClaName } from 'mo/common/className';
+import { classNames, getBEMModifier, prefixClaName } from 'mo/common/className';
 
 type BtnSizeType = 'normal' | 'large';
 export interface IButton extends React.ComponentProps<'a'> {
@@ -10,19 +10,23 @@ export interface IButton extends React.ComponentProps<'a'> {
     size?: BtnSizeType;
 }
 
-export const defaultButtonClassName = 'btn';
+const defaultButtonClassName = prefixClaName('btn');
+const normalButtonClassName = getBEMModifier(defaultButtonClassName, 'normal');
+const largeButtonClassName = getBEMModifier(defaultButtonClassName, 'large');
 
 export function Button(props: React.PropsWithChildren<IButton>) {
-    const { className, children, size = 'normal', ...others } = props;
+    const { className, children, size = 'normal', ...custom } = props;
+
+    const sizeClassName = size === 'large' ? largeButtonClassName : normalButtonClassName;
 
     const claNames = classNames(
-        prefixClaName(defaultButtonClassName),
-        size,
+        defaultButtonClassName,
+        sizeClassName,
         className
     );
 
     return (
-        <a className={claNames} {...others}>
+        <a className={claNames} {...custom}>
             {children}
         </a>
     );

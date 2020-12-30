e94acc17a793a1ce4d3dbc26b6f16b6a36997051
xiaowei <xiaowei@dtstack.com>
2020-12-30 10:27:44 +0800

    fix(tabs): add dragAndDrop HOC

---
+++ src/components/tabs/dragAndDrop.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { DndProvider, createDndContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { PropsWithChildren, useRef } from 'react';
+
+const RNDContext = createDndContext(HTML5Backend);
+
+function DragAndDrop({ children }: PropsWithChildren<{}>): JSX.Element {
+    const manager = useRef(RNDContext);
+    return (
+        <DndProvider manager={manager.current.dragDropManager}>
+            {children}
+        </DndProvider>
+    );
+}
+
+export default DragAndDrop;

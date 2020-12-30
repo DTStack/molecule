9160494a5bd580fc40a936fb734d9b5e923c7cbb
xiaowei <xiaowei@dtstack.com>
2020-12-30 15:56:52 +0800

    refactor: move react docgen into storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,9 @@ module.exports = {
     stories: ['../stories/**/*.stories.tsx'],
     addons: ['@storybook/addon-actions', '@storybook/addon-links'],
     webpackFinal: async (config) => {
+        baseConf.module.rules[0].use.push({
+            loader: require.resolve('react-docgen-typescript-loader'),
+        });
         return merge(config, baseConf);
     },
 };

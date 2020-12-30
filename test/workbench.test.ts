9c84bd4910601b74c0d840b11b662d6b1ba8acc2
xiaowei <xiaowei@dtstack.com>
2020-12-30 15:58:34 +0800

    refactor(test): update importing location

--- test/workbench.test.ts
+++ test/workbench.test.ts
@@ -1,5 +1,5 @@
-// import { MoleculeProvider } from './dist';
-// import { Workbench, Sidebar, Layout, Terminal } from './dist/lib';
+// import { MoleculeProvider } from './lib';
+// import { Workbench, Sidebar, Layout, Terminal } from './lib';
 // import zhCN from './dist/locale/zh_CN';
 // import themeDark from './src/extensions/default-theme-dark';
 

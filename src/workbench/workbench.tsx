ec602cddbdcc880a5d90ef00df4e7528b5062fd9
xiaowei <xiaowei@dtstack.com>
2021-02-03 17:27:08 +0800

    style: prettify code

--- src/workbench/workbench.tsx
+++ src/workbench/workbench.tsx
@@ -14,9 +14,9 @@ import { ID_APP } from 'mo/common/id';
 import { Utils } from '@dtinsight/dt-utils';
 import { APP_PREFIX } from 'mo/common/const';
 
-export interface IWorkbench { }
+export interface IWorkbench {}
 
-export interface IMainBench { }
+export interface IMainBench {}
 
 const mainBenchClassName = prefixClaName('mainBench');
 const workbenchClassName = prefixClaName('workbench');

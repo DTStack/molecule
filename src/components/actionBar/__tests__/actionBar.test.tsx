fab05bce96a4c625a52e103393c340b899b23e20
xiaowei <xiaowei@dtstack.com>
2021-02-19 16:05:31 +0800

    test(actionbar): modified mock data

--- src/components/actionBar/__tests__/actionBar.test.tsx
+++ src/components/actionBar/__tests__/actionBar.test.tsx
@@ -4,15 +4,16 @@ import renderer from 'react-test-renderer';
 
 import ActionBar from '../index';
 
-const mockData = [{
-    id: '1',
-    title: 'mockDataTitle',
-    iconName: 'codicon-add',
-}];
+const mockData = [
+    {
+        id: '1',
+        title: 'mockDataTitle',
+        iconName: 'codicon-add',
+    },
+];
 
 describe('Test ActionBar Component', () => {
     test('Test the ActionBar Snapshot', () => {
-
         const component = renderer.create(<ActionBar data={mockData} />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();

8585549490023634dd9bffc8431129b95597b2b3
xiaowei <xiaowei@dtstack.com>
2021-02-19 15:53:28 +0800

    test(actionbar): add simple Snapshort testing and DOM testing case

--- src/components/actionBar/__tests__/actionBar.test.tsx
+++ src/components/actionBar/__tests__/actionBar.test.tsx
@@ -1,34 +1,25 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 import ActionBar from '../index';
 
-const mockData = [
-    {
-        id: '1',
-        title: 'bar1',
-        iconName: 'codicon-add',
-    },
-    {
-        id: '2',
-        title: 'bar2',
-        iconName: 'codicon-chrome-restore',
-    },
-    {
-        id: '3',
-        title: 'bar3',
-        iconName: 'codicon-check',
-    },
-];
+const mockData = [{
+    id: '1',
+    title: 'mockDataTitle',
+    iconName: 'codicon-add',
+}];
 
 describe('Test ActionBar Component', () => {
+    test('Test the ActionBar Snapshot', () => {
 
-    test('Shows the ActionBar by data Props', () => {
-        expect(1 + 1).toEqual(2);
-        render(<ActionBar data={mockData} />);
-
-        expect(screen.getByTitle(/bar1/)).not.toBeNull();
-
+        const component = renderer.create(<ActionBar data={mockData} />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
     });
 
+    test('Test the ActionBar by the data Props', () => {
+        render(<ActionBar data={mockData} />);
+        expect(screen.getByTitle(/mockDataTitle/)).not.toBeNull();
+    });
 });

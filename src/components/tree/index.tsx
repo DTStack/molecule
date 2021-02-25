5b59ecb0389aaed36f7b6200d3a805325d53af09
zhangtengjin <1257450210@qq.com>
2021-02-25 10:20:06 +0800

    fix: fix ci type validate error
    fix ci type validate error

--- src/components/tree/index.tsx
+++ src/components/tree/index.tsx
@@ -15,8 +15,8 @@ export enum FileTypes {
     FILE = 'file',
     FOLDER = 'folder',
     ROOT = 'rootFolder',
-}
-export type FileType = keyof typeof FileTypes;
+};
+export type FileType = 'file' | 'folder' | 'rootFolder';
 
 export interface ITreeNodeItem {
     name?: string;
@@ -51,11 +51,11 @@ export interface ITreeProps {
     expandedKeys?: Key[];
     defaultCheckedKeys?: Key[];
     checkedKeys?:
-        | Key[]
-        | {
-              checked: Key[];
-              halfChecked: Key[];
-          };
+    | Key[]
+    | {
+        checked: Key[];
+        halfChecked: Key[];
+    };
     defaultSelectedKeys?: Key[];
     selectedKeys?: Key[];
     titleRender?: (node: DataNode) => React.ReactNode;

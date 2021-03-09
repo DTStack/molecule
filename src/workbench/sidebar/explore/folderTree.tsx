48cf9820e0450b1dea020118bb160846214b66fa
zhangtengjin <1257450210@qq.com>
2021-03-09 11:00:49 +0800

    feat: extract TreeViewUtil to help file
    extract TreeViewUtil to help file

--- src/workbench/sidebar/explore/folderTree.tsx
+++ src/workbench/sidebar/explore/folderTree.tsx
@@ -22,7 +22,6 @@ const FolderTree: React.FunctionComponent<IFolderTree> = (
         onClickContextMenu,
         ...restProps
     } = props;
-    console.log('FolderTree => FolderTreeController', props)
     const inputRef = useRef<any>(null);
 
     const contextView = useContextView();

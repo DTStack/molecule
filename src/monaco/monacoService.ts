c2770f8d56f8505af82a6601f4b1e5c199ce6766
xiaowei <xiaowei@dtstack.com>
2021-03-09 20:52:09 +0800

    feat(monaco-editor): override layoutService and quickAccessService
    There encapusulated a monacoService, the default monaco-editor component will be created by it, and
    also you can access ServiceCollection

---
+++ src/monaco/monacoService.ts
@@ -0,0 +1,156 @@
+import 'reflect-metadata';
+import { singleton } from 'tsyringe';
+
+import {
+    IStandaloneEditorConstructionOptions,
+    IStandaloneCodeEditor,
+    StandaloneEditor,
+} from 'monaco-editor/esm/vs/editor/standalone/browser/standaloneCodeEditor';
+
+import {
+    SimpleEditorModelResolverService,
+    SimpleLayoutService,
+} from 'monaco-editor/esm/vs/editor/standalone/browser/simpleServices';
+import { ITextModelService } from 'monaco-editor/esm/vs/editor/common/services/resolverService';
+import {
+    StaticServices,
+    IEditorOverrideServices,
+    DynamicStandaloneServices,
+} from 'monaco-editor/esm/vs/editor/standalone/browser/standaloneServices';
+import { IInstantiationService } from 'monaco-editor/esm/vs/platform/instantiation/common/instantiation';
+import { ICodeEditorService } from 'monaco-editor/esm/vs/editor/browser/services/codeEditorService';
+import { ICommandService } from 'monaco-editor/esm/vs/platform/commands/common/commands';
+import { IContextKeyService } from 'monaco-editor/esm/vs/platform/contextkey/common/contextkey';
+import { IKeybindingService } from 'monaco-editor/esm/vs/platform/keybinding/common/keybinding';
+import { IContextViewService } from 'monaco-editor/esm/vs/platform/contextview/browser/contextView';
+import { IStandaloneThemeService } from 'monaco-editor/esm/vs/editor/standalone/common/standaloneThemeService';
+import { INotificationService } from 'monaco-editor/esm/vs/platform/notification/common/notification';
+import { IConfigurationService } from 'monaco-editor/esm/vs/platform/configuration/common/configuration';
+import { IAccessibilityService } from 'monaco-editor/esm/vs/platform/accessibility/common/accessibility';
+import { IOpenerService } from 'monaco-editor/esm/vs/platform/opener/common/opener';
+import { OpenerService } from 'monaco-editor/esm/vs/editor/browser/services/openerService';
+import { QuickInputService } from 'monaco-editor/esm/vs/platform/quickinput/browser/quickInput';
+import { IQuickInputService } from 'monaco-editor/esm/vs/platform/quickinput/common/quickInput';
+import { ILayoutService } from 'monaco-editor/esm/vs/platform/layout/browser/layoutService';
+import { ServiceCollection } from 'monaco-editor/esm/vs/platform/instantiation/common/serviceCollection';
+
+import { ID_APP } from 'mo/common/id';
+
+export interface IMonacoService {
+    readonly services: ServiceCollection;
+    readonly container: HTMLElement | null;
+    create(
+        domElement: HTMLElement,
+        options?: IStandaloneEditorConstructionOptions,
+        overrides?: IEditorOverrideServices
+    ): IStandaloneCodeEditor;
+}
+
+@singleton()
+export class MonacoService implements IMonacoService {
+    private _services: ServiceCollection;
+    private simpleEditorModelResolverService: SimpleEditorModelResolverService | null = null;
+
+    constructor() {}
+
+    get services() {
+        if (!this._services) {
+            this._services = this.createStandaloneServices();
+        }
+        return this._services;
+    }
+
+    get container() {
+        return document.getElementById(ID_APP);
+    }
+
+    // private mergeEditorServices(overrides?: IEditorOverrideServices) {
+    //     if (overrides) {
+    //         const services = this.services;
+    //         for (const serviceId in overrides) {
+    //             if (serviceId) {
+    //                 const service = services.get(serviceId);
+    //                 if (service && overrides[serviceId]){
+    //                     services.set(serviceId, overrides[serviceId]);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    public create(
+        domElement: HTMLElement,
+        options?: IStandaloneEditorConstructionOptions,
+        overrides?: IEditorOverrideServices
+    ): IStandaloneCodeEditor {
+        
+        const services = this.services;
+
+        const standaloneEditor = new StandaloneEditor(
+            domElement,
+            options,
+            services,
+            services.get(IInstantiationService),
+            services.get(ICodeEditorService),
+            services.get(ICommandService),
+            services.get(IContextKeyService),
+            services.get(IKeybindingService),
+            services.get(IContextViewService),
+            services.get(IStandaloneThemeService),
+            services.get(INotificationService),
+            services.get(IConfigurationService),
+            services.get(IAccessibilityService)
+        );
+
+        if (this.simpleEditorModelResolverService) {
+            this.simpleEditorModelResolverService.setEditor(standaloneEditor);
+        }
+        return standaloneEditor;
+    }
+
+    private createStandaloneServices(): ServiceCollection {
+        const services = new DynamicStandaloneServices(this.container);
+
+        this.overrideServices(services);
+
+        if (!services.has(ITextModelService)) {
+            this.simpleEditorModelResolverService = new SimpleEditorModelResolverService(
+                StaticServices.modelService.get()
+            );
+            services.set(
+                ITextModelService,
+                this.simpleEditorModelResolverService
+            );
+        }
+
+        if (!services.has(IOpenerService)) {
+            services.set(
+                IOpenerService,
+                new OpenerService(
+                    services.get(ICodeEditorService),
+                    services.get(ICommandService)
+                )
+            );
+        }
+
+        return services;
+    }
+
+    private overrideServices(services) {
+        const instantiationService = services.get(IInstantiationService);
+
+        const quickInputService = instantiationService.createInstance(
+            QuickInputService
+        );
+        const layoutService = new SimpleLayoutService(
+            StaticServices.codeEditorService.get(ICodeEditorService),
+            this.container
+        );
+        
+        // Override layoutService
+        services.set(ILayoutService, layoutService);
+
+        // Override quickPickService
+        services.set(IQuickInputService, quickInputService);
+    }
+}
---
+++ src/monaco/selectColorThemeAction.ts
@@ -0,0 +1,119 @@
+import { localize } from 'monaco-editor/esm/vs/nls';
+import {
+    IQuickInputService,
+    QuickPickInput,
+} from 'monaco-editor/esm/vs/platform/quickinput/common/quickInput';
+import { colorThemeService } from 'mo/services';
+import { IColorTheme } from 'mo/model/colorTheme';
+import {
+    EditorAction,
+    registerEditorAction,
+} from 'monaco-editor/esm/vs/editor/browser/editorExtensions';
+import * as monaco from 'monaco-editor';
+import { KeyChord } from 'monaco-editor/esm/vs/base/common/keyCodes';
+
+export class SelectColorThemeAction extends EditorAction {
+    static readonly ID = 'workbench.action.selectTheme';
+    static readonly LABEL = localize('selectTheme.label', 'Color Theme');
+    private quickInputService: IQuickInputService;
+
+    constructor() {
+        super({
+            id: SelectColorThemeAction.ID,
+            label: SelectColorThemeAction.LABEL,
+            alias: 'Color Theme',
+            precondition: undefined,
+            kbOpts: {
+                kbExpr: undefined,
+                // eslint-disable-next-line new-cap
+                primary: KeyChord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_K),
+            },
+        });
+    }
+
+    run(accessor): Promise<void> {
+        this.quickInputService = accessor.get(IQuickInputService);
+        const themes = colorThemeService.getThemes();
+        const currentTheme = colorThemeService.getColorTheme();
+     
+        const picks: QuickPickInput<IColorTheme>[] = [...toEntries(themes)];
+
+        let selectThemeTimeout: number | undefined;
+
+        const selectTheme = (theme: IColorTheme, applyTheme: boolean) => {
+            if (selectThemeTimeout) {
+                clearTimeout(selectThemeTimeout);
+            }
+            selectThemeTimeout = window.setTimeout(
+                () => {
+                    selectThemeTimeout = undefined;
+                    const themeId =
+                        theme && theme.id !== undefined
+                            ? theme.id
+                            : currentTheme.id;
+                    colorThemeService.applyTheme(themeId);
+                },
+                applyTheme ? 0 : 200
+            );
+        };
+
+        return new Promise((resolve) => {
+            let isCompleted = false;
+
+            const autoFocusIndex = picks.findIndex(
+                (p) => p.id === currentTheme.id
+            );
+            const quickPick = this.quickInputService.createQuickPick<IColorTheme>();
+            quickPick.items = picks;
+            quickPick.placeholder = localize(
+                'themes.selectTheme',
+                'Select Color Theme (Up/Down Keys to Preview)'
+            );
+            quickPick.activeItems = [picks[autoFocusIndex]];
+            quickPick.canSelectMany = false;
+            quickPick.onDidAccept((_) => {
+                const theme = quickPick.activeItems[0];
+                if (theme) {
+                    selectTheme(theme, true);
+                }
+                isCompleted = true;
+                quickPick.hide();
+                resolve();
+            });
+
+            quickPick.onDidChangeActive((themes) =>
+                selectTheme(themes[0], false)
+            );
+            quickPick.onDidHide(() => {
+                if (!isCompleted) {
+                    selectTheme(currentTheme, true);
+                    resolve();
+                }
+            });
+
+            quickPick.show();
+        });
+    }
+}
+
+function toEntries(
+    themes: Array<IColorTheme>,
+    label?: string
+): QuickPickInput<IColorTheme>[] {
+    const toEntry = (theme: IColorTheme): IColorTheme => ({
+        id: theme.id,
+        label: theme.label,
+        description: theme.description,
+    });
+    const sorter = (t1: IColorTheme, t2: IColorTheme) =>
+        t1.label?.localeCompare(t2.label);
+    const entries: QuickPickInput<IColorTheme>[] = themes
+        .map(toEntry)
+        .sort(sorter);
+    if (entries.length > 0 && label) {
+        entries.unshift({ type: 'separator', label });
+    }
+    return entries;
+}
+
+registerEditorAction(SelectColorThemeAction);

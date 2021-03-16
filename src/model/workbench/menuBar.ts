0e5a57d9a242f2d451a35a43cb6be8ce57c1db7f
mumiao <mumiao@dtstack.com>
2021-03-16 20:17:40 +0800

    feat: init menuData in menuBarModel

--- src/model/workbench/menuBar.ts
+++ src/model/workbench/menuBar.ts
@@ -29,6 +29,111 @@ export const MENU_VIEW_SIDEBAR = 'workbench.action.showSideBar';
 export const MENU_VIEW_ACTIVITYBAR = 'workbench.action.showActivityBar';
 export const MENU_VIEW_STATUSBAR = 'workbench.action.showStatusBar';
 
+const builtMenuData= [
+    {
+        id: 'File',
+        name: 'File',
+        data: [
+            {
+                id: 'New File',
+                name: 'New File',
+            },
+            {
+                id: 'OpenFile',
+                name: 'Open',
+            },
+        ],
+    },
+    {
+        id: 'Edit',
+        name: 'Edit',
+        data: [
+            {
+                id: MENU_FILE_UNDO,
+                name: 'Undo',
+            },
+            {
+                id: MENU_FILE_REDO,
+                name: 'Redo',
+            },
+        ],
+    },
+    {
+        id: 'Selection',
+        name: 'Selection',
+        data: [
+            {
+                id: 'SelectAll',
+                name: 'Select All',
+            },
+            {
+                id: 'CopyLineUp',
+                name: 'Copy Line Up',
+            },
+        ],
+    },
+    {
+        id: 'View',
+        name: 'View',
+        data: [
+            {
+                id: 'Command Palette',
+                name: 'Command Palette',
+            },
+            {
+                id: 'OpenView',
+                name: 'Open View',
+            },
+            {
+                id: 'Appearance',
+                name: 'Appearance',
+                data: [
+                    {
+                        icon: 'check',
+                        id: MENU_VIEW_MENUBAR,
+                        name: 'Show Menu Bar',
+                    },
+                    {
+                        icon: 'check',
+                        id: MENU_VIEW_SIDEBAR,
+                        name: 'Show Side Bar',
+                    },
+                    {
+                        icon: 'check',
+                        id: MENU_VIEW_STATUSBAR,
+                        name: 'Show Status Bar',
+                    },
+                    {
+                        icon: 'check',
+                        id: MENU_VIEW_ACTIVITYBAR,
+                        name: 'Show Activity Bar',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        id: 'Run',
+        name: 'Run',
+        data: [
+            {
+                id: 'RunTask',
+                name: 'Run Task',
+            },
+        ],
+    },
+    {
+        id: 'Help',
+        name: 'Help',
+        data: [
+            {
+                id: 'About',
+                name: 'About',
+            },
+        ],
+    },
+];
+
 export const undoRedoMenu = [
     {
         id: MENU_FILE_UNDO,
@@ -44,7 +149,7 @@ export class MenuBarModel implements IMenuBar {
     public data: IMenuBarItem[];
     public hidden = false;
 
-    constructor(data: IMenuBarItem[] = [], hidden = false) {
+    constructor(data: IMenuBarItem[] = builtMenuData, hidden = false) {
         this.data = data;
         this.hidden = hidden;
     }
--- src/workbench/menuBar/menuBar.tsx
+++ src/workbench/menuBar/menuBar.tsx
@@ -2,133 +2,17 @@ import 'mo/workbench/menuBar/style.scss';
 import * as React from 'react';
 import { getBEMElement, prefixClaName } from 'mo/common/className';
 import { IMenuBar } from 'mo/model/workbench/menuBar';
-import {
-    MENU_FILE_REDO,
-    MENU_FILE_UNDO,
-    MENU_VIEW_ACTIVITYBAR,
-    MENU_VIEW_MENUBAR,
-    MENU_VIEW_STATUSBAR,
-    MENU_VIEW_SIDEBAR,
-} from 'mo/model/workbench/menuBar';
+import { IMenuBarController } from 'mo/controller/menuBar';
+
 import { Menu } from 'mo/components/menu';
 import { DropDown } from 'mo/components/dropdown';
 import { Icon } from 'mo/components/icon';
-import { IMenuBarController } from 'mo/controller/menuBar';
-
-export interface IMenuBarProps {
-    // menuBar: IMenuBar;
-}
-
-const initialMenuData = [
-    {
-        id: 'File',
-        name: 'File',
-        data: [
-            {
-                id: 'New File',
-                name: 'New File',
-            },
-            {
-                id: 'OpenFile',
-                name: 'Open',
-            },
-        ],
-    },
-    {
-        id: 'Edit',
-        name: 'Edit',
-        data: [
-            {
-                id: MENU_FILE_UNDO,
-                name: 'Undo',
-            },
-            {
-                id: MENU_FILE_REDO,
-                name: 'Redo',
-            },
-        ],
-    },
-    {
-        id: 'Selection',
-        name: 'Selection',
-        data: [
-            {
-                id: 'SelectAll',
-                name: 'Select All',
-            },
-            {
-                id: 'CopyLineUp',
-                name: 'Copy Line Up',
-            },
-        ],
-    },
-    {
-        id: 'View',
-        name: 'View',
-        data: [
-            {
-                id: 'Command Palette',
-                name: 'Command Palette',
-            },
-            {
-                id: 'OpenView',
-                name: 'Open View',
-            },
-            {
-                id: 'Appearance',
-                name: 'Appearance',
-                data: [
-                    {
-                        icon: 'check',
-                        id: MENU_VIEW_MENUBAR,
-                        name: 'Show Menu Bar',
-                    },
-                    {
-                        icon: 'check',
-                        id: MENU_VIEW_SIDEBAR,
-                        name: 'Show Side Bar',
-                    },
-                    {
-                        icon: 'check',
-                        id: MENU_VIEW_STATUSBAR,
-                        name: 'Show Status Bar',
-                    },
-                    {
-                        icon: 'check',
-                        id: MENU_VIEW_ACTIVITYBAR,
-                        name: 'Show Activity Bar',
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        id: 'Run',
-        name: 'Run',
-        data: [
-            {
-                id: 'RunTask',
-                name: 'Run Task',
-            },
-        ],
-    },
-    {
-        id: 'Help',
-        name: 'Help',
-        data: [
-            {
-                id: 'About',
-                name: 'About',
-            },
-        ],
-    },
-];
 
 const defaultClassName = prefixClaName('menuBar');
 const actionClassName = getBEMElement(defaultClassName, 'action');
 
 function MenuBar(props: IMenuBar & IMenuBarController) {
-    const { onClick } = props;
+    const { data, onClick } = props;
     const childRef = React.useRef();
     const handleClick = (e: React.MouseEvent, item) => {
         onClick?.(e, item);
@@ -138,7 +22,7 @@ function MenuBar(props: IMenuBar & IMenuBarController) {
         <Menu
             onClick={handleClick}
             style={{ width: 200 }}
-            data={initialMenuData}
+            data={data}
         />
     );
     return (

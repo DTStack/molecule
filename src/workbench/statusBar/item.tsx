c38b182e7343e3c4c468cfa08624385a57a4abaa
linhe <linhe@dtstack.com>
2021-03-19 15:38:17 +0800

    feat(status/bar): modify status bar item

--- src/workbench/statusBar/item.tsx
+++ src/workbench/statusBar/item.tsx
@@ -2,14 +2,11 @@ import { classNames } from 'mo/common/className';
 import { IStatusBarItem } from 'mo/model/workbench/statusBar';
 import * as React from 'react';
 import { memo } from 'react';
-import { statusBarService } from 'mo/services';
 import { itemClassName } from './base';
 
 function StatusItem(props: IStatusBarItem) {
     const { className, onClick, name, data, render, ...extra } = props;
     const clsName = classNames(itemClassName, className);
-    const { hidden } = statusBarService.getState();
-    const display = hidden ? 'none' : 'block';
     const events = {
         onClick: function (e: React.MouseEvent) {
             onClick?.(e, props);
@@ -17,7 +14,7 @@ function StatusItem(props: IStatusBarItem) {
     };
 
     return (
-        <div className={clsName} {...extra} style={{ display }}>
+        <div className={clsName} {...extra}>
             <a tabIndex={-1} title={name} {...events}>
                 {render ? render(props) : name}
             </a>

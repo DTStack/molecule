94fda0d51a796985ea9169db62e84a345a8bcd76
zhangtengjin <1257450210@qq.com>
2021-04-12 15:22:12 +0800

    feat: connect state and view by connect method
    connect state and view by connect method

--- src/workbench/sidebar/search/index.tsx
+++ src/workbench/sidebar/search/index.tsx
@@ -1,7 +1,14 @@
 import { connect } from 'mo/react';
-import { searchService } from 'mo/services';
+import { searchService, folderTreeService } from 'mo/services';
 import { searchController } from 'mo/controller';
 import SearchPanel from './searchPanel';
 
-const SearchPanelView = connect(searchService, SearchPanel, searchController);
+const SearchPanelView = connect(
+    {
+        search: searchService,
+        folderTree: folderTreeService,
+    },
+    SearchPanel,
+    searchController
+);
 export { SearchPanelView, SearchPanel };

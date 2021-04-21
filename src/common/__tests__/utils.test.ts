562cd4aa9c67502eee6612c1a5858c266e8b0088
xiaowei <wewoor@gmail.com>
2021-04-21 17:18:01 +0800

    test: add case for normalizeFlattedObject

---
+++ src/common/__tests__/utils.test.ts
@@ -0,0 +1,38 @@
+import { normalizeFlattedObject } from '../utils';
+
+describe('Test Utils', () => {
+    test('The normalizeFlattedObject function', () => {
+        const expected = {
+            workbench: {
+                colorTheme: 'vs',
+                activityBar: {
+                    hidden: false,
+                    userAccountItem: true,
+                },
+                panel: {
+                    hidden: false,
+                },
+                sidebar: {
+                    hidden: false,
+                },
+                statusBar: {
+                    hidden: false,
+                },
+                menuBar: {
+                    hidden: false,
+                },
+            },
+        };
+        const testData = {
+            'workbench.colorTheme': 'vs',
+            'workbench.activityBar.hidden': false,
+            'workbench.activityBar.userAccountItem': true,
+            'workbench.panel.hidden': false,
+            'workbench.sidebar.hidden': false,
+            'workbench.statusBar.hidden': false,
+            'workbench.menuBar.hidden': false,
+        };
+        const normalized = normalizeFlattedObject(testData);
+        expect(normalized).toEqual(expected);
+    });
+});

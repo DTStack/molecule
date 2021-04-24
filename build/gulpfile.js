a12eb349f1c575f7c1c1d65d16bb6879ac192c43
xiaowei <wewoor@gmail.com>
2021-04-24 14:35:27 +0800

    build: using gulp compile ts to esm modules

---
+++ build/gulpfile.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const through = require('through2');
+const tsProject = ts.createProject('../tsconfig.build.json');
+const output = '../esm';
+
+function replacePath(code, filePath, importOptions) {
+    const sourcePath = path.dirname(filePath);
+    const targetPath = path.resolve(output);
+    const relativePath = path.relative(sourcePath, targetPath);
+    const finalPath = relativePath ? './' + relativePath + '/' : './';
+    return code.replace(/([from|import]\s+)'mo\//g, "$1'" + finalPath);
+}
+
+function relativeImport(compilerOptions) {
+    return through.obj(function (file, encoding, cb) {
+        if (!file.contents) {
+            return;
+        }
+        let content = file.contents.toString('utf-8');
+        content = replacePath(
+            content,
+            file.history.toString(),
+            compilerOptions
+        );
+        file.contents = Buffer.from(content);
+        this.push(file);
+        cb();
+    });
+}
+
+gulp.task('build:esm', function () {
+    return tsProject
+        .src()
+        .pipe(tsProject())
+        .pipe(relativeImport(tsProject.config.compilerOptions))
+        .on('error', function (error, callback) {
+            console.error(error.stack);
+        })
+        .pipe(gulp.dest(output));
+});

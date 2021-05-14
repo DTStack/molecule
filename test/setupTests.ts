e4f35e8ff4710589c63ad9fe118343e9da92b7db
xiaowei <wewoor@gmail.com>
2021-05-14 18:12:46 +0800

    test: mock the mo/monaco path and the MonacoService

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,5 +1,16 @@
-jest.mock('mo/monaco/monacoService', () => ({
-    monacoService: {
-        create: function (dom, options, override) {},
-    },
-}));
+jest.mock('mo/monaco/monacoService', () => {
+    function MonacoService() {}
+    const getter = { get: () => {} };
+
+    MonacoService.prototype.create = function (dom, options, override) {};
+    MonacoService.prototype.initWorkspace = function (
+        container: HTMLElement
+    ) {};
+    MonacoService.prototype.services = getter;
+    MonacoService.prototype.commandService = getter;
+    MonacoService.prototype.container = getter;
+
+    return {
+        MonacoService: MonacoService,
+    };
+});

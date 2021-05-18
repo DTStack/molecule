6d0fdbc1a6382e926053bea1f48be94f388b93ac
xiaowei <wewoor@gmail.com>
2021-05-18 14:25:13 +0800

    fix(monaco): import default language contributions

--- src/monaco/index.ts
+++ src/monaco/index.ts
@@ -1,3 +1,8 @@
+import 'monaco-editor/esm/vs/language/typescript/monaco.contribution';
+import 'monaco-editor/esm/vs/language/css/monaco.contribution';
+import 'monaco-editor/esm/vs/language/json/monaco.contribution';
+import 'monaco-editor/esm/vs/language/html/monaco.contribution';
+import 'monaco-editor/esm/vs/basic-languages/monaco.contribution';
 /**
  * This module is overwrite the original monaco-editor/esm/vs/editor/editor.main.js
  */

d02f5527e81c5c2d605b946836bd8645caba889a
xiaowei <wewoor@gmail.com>
2021-05-24 11:47:53 +0800

    feat: update builtin color theme

--- src/extensions/vscode-palenight-theme/index.ts
+++ src/extensions/vscode-palenight-theme/index.ts
@@ -4,12 +4,21 @@ import { IExtension } from 'mo/model/extension';
 const paleNightColorThemeExtension: IExtension = require('./package.json');
 
 // Default
+const themeColors: IColorTheme = require('./themes/palenight.json');
 const themeItalicColors: IColorTheme = require('./themes/palenight-italic.json');
+const themeOperatorColors: IColorTheme = require('./themes/palenight-operator.json');
+const themeMildContrastColors: IColorTheme = require('./themes/palenight-mild-contrast.json');
 
 const themes = paleNightColorThemeExtension.contributes?.themes || [];
 
+const themeZero = themes[0];
 const themeOne = themes[1];
+const themeTwo = themes[2];
+const themeThree = themes[3];
 
+themes[0] = Object.assign({}, themeZero, themeColors);
 themes[1] = Object.assign({}, themeOne, themeItalicColors);
+themes[2] = Object.assign({}, themeTwo, themeOperatorColors);
+themes[3] = Object.assign({}, themeThree, themeMildContrastColors);
 
 export { paleNightColorThemeExtension };

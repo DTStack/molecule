52c7a5f8bbd6a1735fd96e2c358c4e41b461d184
xiaowei <wewoor@gmail.com>
2021-05-26 17:11:37 +0800

    refactor: remove useless overwrite for monaco

--- src/monaco/index.ts
+++ src/monaco/index.ts
@@ -1,29 +1,24 @@
+/**
+ * This module is overwrite the original monaco-editor/esm/vs/editor/editor.main.js
+ */
+
 import 'monaco-editor/esm/vs/language/typescript/monaco.contribution';
 import 'monaco-editor/esm/vs/language/css/monaco.contribution';
 import 'monaco-editor/esm/vs/language/json/monaco.contribution';
 import 'monaco-editor/esm/vs/language/html/monaco.contribution';
 import 'monaco-editor/esm/vs/basic-languages/monaco.contribution';
-/**
- * This module is overwrite the original monaco-editor/esm/vs/editor/editor.main.js
- */
+
 import 'monaco-editor/esm/vs/editor/editor.all';
 import 'monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp';
 import 'monaco-editor/esm/vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard';
 import 'monaco-editor/esm/vs/editor/standalone/browser/inspectTokens/inspectTokens';
 import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneHelpQuickAccess';
 
-/**
- * TODO Disable the default GotoLineQuickAccess and GotoSymbolQuickAccess services temporarily
- */
-// import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess';
-// import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoSymbolQuickAccess';
-// import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess';
+import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess';
+import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoSymbolQuickAccess';
+import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess';
 
 import 'monaco-editor/esm/vs/editor/standalone/browser/referenceSearch/standaloneReferenceSearch';
 import 'monaco-editor/esm/vs/editor/standalone/browser/toggleHighContrast/toggleHighContrast';
 
-/**
- * The original editor.api.js file will automatic load standalone quickAccess services other services, so there customize the api
- * export * from 'monaco-editor/esm/vs/editor/editor.api.js';
- */
-export * from './monaco.api';
+export * from 'monaco-editor/esm/vs/editor/editor.api.js';

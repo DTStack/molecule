1fd72f3d7613e7a88ac0c781a80a6791ad876355
xiaowei <wewoor@gmail.com>
2021-06-08 10:00:10 +0800

    fix: remove useless

--- src/extensions/editor/index.tsx
+++ src/extensions/editor/index.tsx
@@ -1,16 +1,7 @@
 import { IExtensionService } from 'mo/services';
 import { IExtension } from 'mo/model/extension';
-// import molecule from 'mo';
-// import { getEditorInitialActions, getEditorInitialMenu, IEditor } from 'mo/model';
 
-function init() {
-    // const state: IEditor = molecule.editor.getState();
-    // molecule.editor.setState({
-    //     ...state,
-    //     actions: getEditorInitialActions(),
-    //     menu: getEditorInitialMenu(),
-    // });
-}
+function init() {}
 
 export const ExtendsEditor: IExtension = {
     activate(extensionCtx: IExtensionService) {

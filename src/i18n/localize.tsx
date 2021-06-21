ad1bbc14dddcef720b0185d537cf8d93f360955e
野迂迂 <yangwei1@outlook.com>
2021-06-21 09:46:16 +0800

    feat(i18n): localize support to provide dynamic replace text (#186)
    * feat(i18n): localize support to provide dynamic replace text
    
    * fix(scrollable): remove warning about scrollable div key

--- src/i18n/localize.tsx
+++ src/i18n/localize.tsx
@@ -9,10 +9,37 @@ export interface ILocalizeProps {
     defaultValue?: string;
 }
 
-export function localize(sourceKey: string, defaultValue: string): any {
-    const localizedValue = container
+const LOCALIZE_REPLACED_WORD = '${i}';
+
+/**
+ * Returns the international text located by source key，or the default value if it is not find
+ * For examples:
+ * ```ts
+ * localize('id','default value'); // hello ${i}, ${i}
+ * localize('id','default value', 'world'); // hello world, ${i}
+ * localize('id','default value', 'world', 'molecule'); // hello world, molecule
+ * ```
+ * @param sourceKey The key value located in the source internaional text
+ * @param defaultValue The default value to be used when not find the international text
+ * @param args If provided, it will used as the values to be replaced in the international text
+ * @returns
+ */
+export function localize(
+    sourceKey: string,
+    defaultValue: string,
+    ...args: string[]
+): any {
+    let localizedValue = container
         .resolve(LocaleService)
         .localize(sourceKey, defaultValue);
+    if (args.length) {
+        args.forEach((replacedVal) => {
+            localizedValue = localizedValue.replace(
+                LOCALIZE_REPLACED_WORD,
+                replacedVal
+            );
+        });
+    }
     return localizedValue;
 }
 

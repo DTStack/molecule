faf582f12c4c7a1875b39574561a9dc7369daa74
mortalYoung <yangwei1@outlook.com>
2021-07-14 14:50:26 +0800

    feat: problem panel support to render more informations

--- src/workbench/problems/paneView/index.tsx
+++ src/workbench/problems/paneView/index.tsx
@@ -1,23 +1,53 @@
 import * as React from 'react';
-import { prefixClaName } from 'mo/common/className';
+import { getBEMElement, prefixClaName } from 'mo/common/className';
 import TreeView from 'mo/components/tree';
+import { localize } from 'mo/i18n/localize';
 
 const defaultClassName = prefixClaName('problems');
+const treeClassName = getBEMElement(defaultClassName, 'treeview');
+const treeNodeClassName = getBEMElement(treeClassName, 'treeNode');
+const treeNodeBadgeClassName = getBEMElement(treeNodeClassName, 'badge');
+const treeLeafClassName = getBEMElement(treeClassName, 'treeLeaf');
+const treeLeafSubInfoClassName = getBEMElement(treeLeafClassName, 'subInfo');
 
 function ProblemsPaneView(props: any) {
     const { data } = props;
-    const renderTitle = (item: any): any => {
-        const { name, children, value } = item;
+    if (!data?.length)
         return (
-            <span>
-                {children && children.length ? name : value.code}{' '}
-                <span>({children.length})</span>
-            </span>
+            <div style={{ margin: '0 18px', userSelect: 'none' }}>
+                {localize(
+                    'panel.problems.empty',
+                    'No problems have been detected in the workspace.'
+                )}
+            </div>
         );
-    };
+
     return (
         <div className={defaultClassName}>
-            <TreeView data={data} renderTitle={renderTitle} />
+            <TreeView
+                className={treeClassName}
+                data={data}
+                renderTitle={({ children, name, value }, _, isLeaf) => {
+                    return !isLeaf ? (
+                        <span className={treeNodeClassName}>
+                            {value.code}
+                            <span className={treeNodeBadgeClassName}>
+                                {children?.length}
+                            </span>
+                        </span>
+                    ) : (
+                        <span className={treeLeafClassName}>
+                            {value.message}
+                            <span className={treeLeafSubInfoClassName}>
+                                {value.code}
+                            </span>
+                            <span className={treeLeafSubInfoClassName}>
+                                [{value.startLineNumber}, {value.startColumn}]
+                            </span>
+                        </span>
+                    );
+                }}
+            />
         </div>
     );
 }

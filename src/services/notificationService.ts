03a6b3360d16fea1b9d7d30df7fdd6fc504aa66d
xiaowei <wewoor@gmail.com>
2021-07-29 14:14:00 +0800

    docs: add Comments for the NotificationService interface

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -11,9 +11,24 @@ import { searchById } from './helper';
 import { randomId } from 'mo/common/utils';
 
 export interface INotificationService extends Component<INotification> {
+    /**
+     * Add new notification items
+     * @param items
+     */
     add<T>(items: INotificationItem<T>[]): null | INotificationItem<T>[];
+    /**
+     * Remove the specific notification item by id
+     * @param id
+     */
     remove(id: number): void;
+    /**
+     * Update the specific notification item
+     * @param item notification item, the id field is required
+     */
     update<T>(item: INotificationItem<T>): null | INotificationItem<T>;
+    /**
+     * Toggle the Problems view between display and hidden
+     */
     toggleProblems(): void;
 }
 

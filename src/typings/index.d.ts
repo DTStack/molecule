44b5dd0fe0a73740d665d007a2ea57eb3b1f688d
xiaowei <wewoor@gmail.com>
2021-08-04 13:52:49 +0800

    refactor(typings): remove the useless code

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -3,12 +3,6 @@
  */
 declare let __DEVELOPMENT__: boolean;
 
-/**
- * Global window object
- */
-declare const window: Window;
-
-declare const define: any;
 interface HTMLElementProps<T = any> {
     id?: string;
     title?: string;

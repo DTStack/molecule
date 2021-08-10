1b72922a25d21e60cd439d6cb26c68a172adf3e0
xiaowei <wewoor@gmail.com>
2021-08-10 16:28:57 +0800

    test(contextmenu): show and hide logic

---
+++ src/components/contextMenu/__tests__/contextMenu.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { IContextMenu, useContextMenu } from '../index';
+
+describe('Test ContextMenu Component', () => {
+    const container = render(<div data-testid="anchor"></div>);
+    const anchorEle = container.getByTestId('anchor');
+    let contextMenu: IContextMenu | undefined;
+
+    test('Test the useContextMenu method', () => {
+        contextMenu = useContextMenu({
+            anchor: anchorEle,
+            render() {
+                return <span data-testid="menuitem">Test context menu</span>;
+            },
+        });
+        expect(contextMenu).not.toBeUndefined();
+    });
+
+    test('Test the useContextMenu show method', () => {
+        contextMenu?.show({
+            x: anchorEle.offsetTop,
+            y: anchorEle.offsetLeft,
+        });
+        const content = container.getByTestId('menuitem');
+        expect(content).not.toBeUndefined();
+        expect(contextMenu?.view?.style.visibility).toEqual('visible');
+    });
+
+    test('Test the useContextMenu hide method', () => {
+        contextMenu?.hide();
+        expect(contextMenu?.view?.style.visibility).toEqual('hidden');
+    });
+});

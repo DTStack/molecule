77b9d9b316cd0a781b822f39e7478489803a0372
xiaowei <wewoor@gmail.com>
2021-08-12 16:35:23 +0800

    test: the Icon component

---
+++ src/components/icon/__tests__/icon.test.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import { Icon } from '../index';
+
+describe('Test Icon Component', () => {
+    test('Match the Icon snapshot', () => {
+        const component = renderer.create(<Icon type="github" />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('Click the Icon', () => {
+        const mockCallback = jest.fn();
+        const body = render(
+            <Icon data-testid="myIcon" type="github" onClick={mockCallback} />
+        );
+        fireEvent.click(body.getByTestId('myIcon'));
+        expect(mockCallback).toHaveBeenCalled();
+    });
+
+    test('Icon type is string', () => {
+        const expected = 'beaker';
+        const body = render(<Icon data-testid="myIcon" type={expected} />);
+        expect(body.getByTestId('myIcon').className).toContain(expected);
+    });
+
+    test('Icon type is JSX.Element', () => {
+        const expected = 'codicon-beaker';
+        const body = render(
+            <Icon type={<span data-testid="myIcon" className={expected} />} />
+        );
+        expect(body.getByTestId('myIcon').className).toContain(expected);
+    });
+
+    test('Spin a Icon', () => {
+        const body = render(<Icon data-testid="myIcon" type="beaker ~spin" />);
+        expect(body.getByTestId('myIcon').className).toContain('spin');
+    });
+});

1262429f0ae71a63ffcdc4e7db938d8b5ec8ad24
xiaowei <wewoor@gmail.com>
2021-08-12 19:51:45 +0800

    refactor: update the api of List component

---
+++ src/components/list/__tests__/list.test.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import { List, Item } from '../index';
+import { useState } from 'react';
+
+function TestList(props) {
+    return (
+        <List 
+            data-testid="myList" 
+            className="testList" 
+            mode="horizontal" 
+            current="2"
+            {...props}
+        >
+            <Item id="1" disabled data-testid="myLi1">1</Item>
+            <Item id="2" data-testid="myLi2">2</Item>
+        </List>
+    )
+}
+
+describe('Test the List Component', () => {
+    test('Match the List snapshot', () => {
+        const component = renderer.create(<TestList />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('Click the List item', () => {
+        const mockCallback = jest.fn();
+        const wrapper = render(
+            <TestList onClick={mockCallback} />
+        );
+        fireEvent.click(wrapper.getByTestId('myLi1'));
+        expect(mockCallback).toHaveBeenCalled();
+    });
+
+    test('Select the List item', () => {
+        const mockCallback = jest.fn();
+        const wrapper = render(
+            <TestList onSelect={mockCallback} />
+        );
+        fireEvent.click(wrapper.getByTestId('myLi1'));
+        expect(mockCallback).toHaveBeenCalled();
+    });
+
+    test('Set the List to horizontal mode', () => {
+        const wrapper = render(
+            <TestList/>
+        );
+        expect(wrapper.getByTestId('myList').className).toContain('horizontal');
+    });
+
+    test('Default to active the List item', () => {
+        const wrapper = render(
+            <TestList/>
+        );
+        expect(wrapper.getByTestId('myLi2').className).toContain('active');
+    });
+
+    test('Change the active the item', () => {
+        const wrapper = render(
+            <TestList/>
+        );
+        fireEvent.click(wrapper.getByTestId('myLi1'));
+        expect(wrapper.getByTestId('myLi1').className).toContain('active');
+    });
+
+    test('Change the current active the item', () => {
+
+        const { rerender, getByTestId } = render(
+            <TestList current="1" />
+        );
+
+        expect(getByTestId('myLi1').className).toContain('active');
+
+        rerender(<TestList current="2" />);
+
+        expect(getByTestId('myLi2').className).toContain('active');
+
+    });
+
+    test('Default to disable the List item', () => {
+        const wrapper = render(
+            <TestList/>
+        );
+        expect(wrapper.getByTestId('myLi1').className).toContain('disabled');
+    });
+});

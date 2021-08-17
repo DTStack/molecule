da6dfac2c047187a397c6f325ac5d710eb54ce05
mortalYoung <yangwei1@outlook.com>
2021-08-17 17:06:39 +0800

    test: add unit test for Tabs component

---
+++ src/components/tabs/__tests__/tab.test.tsx
@@ -0,0 +1,190 @@
+import React from 'react';
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/react';
+import {
+    ITabEvent,
+    ITabProps,
+    Tab,
+    tabItemActiveClassName,
+    tabItemClassName,
+} from '../tab';
+import DragAndDrop from '../dragAndDrop';
+
+const tabData = {
+    id: '1',
+    active: false,
+    index: 1,
+    name: 'test',
+};
+
+function DTab(args: ITabProps & ITabEvent) {
+    return (
+        <DragAndDrop>
+            <Tab {...tabData} {...args} />
+        </DragAndDrop>
+    );
+}
+
+export function dragToTargetNode(source: HTMLElement, target: HTMLElement) {
+    fireEvent.dragStart(source);
+    fireEvent.dragEnter(target);
+    fireEvent.dragOver(target);
+    fireEvent.drop(target);
+}
+
+function mockClientOffset(boundingRectSize: number, size: number) {
+    // @ts-ignore
+    HTMLDivElement.prototype.getBoundingClientRect = jest.fn(() => ({
+        right: boundingRectSize,
+        left: boundingRectSize,
+    }));
+
+    // @ts-ignore
+    Event.prototype.clientX = size;
+    // @ts-ignore
+    Event.prototype.clientY = size;
+}
+
+afterEach(cleanup);
+
+describe('The Tab Component', () => {
+    test('Should render name in tab', () => {
+        const { container } = render(<DTab />);
+        const wrapper = container.querySelector(`.${tabItemClassName}`)!;
+        expect(wrapper.innerHTML).toBe('test');
+    });
+
+    test('Should active classNames when active tab', () => {
+        const { container } = render(<DTab active />);
+        const wrapper = container.querySelector(`.${tabItemClassName}`)!;
+        expect(wrapper.classList).toContain(tabItemActiveClassName);
+    });
+
+    test('Should render extra when editable and closable', async () => {
+        const mockFn = jest.fn();
+        const { container } = render(
+            <DTab editable closable onCloseTab={mockFn} />
+        );
+        const wrapper = container.querySelector(`.${tabItemClassName}`)!;
+
+        expect(wrapper.childElementCount).toBe(2);
+
+        fireEvent.mouseOver(wrapper);
+        fireEvent.click(wrapper.children[0].firstChild!);
+        fireEvent.click(wrapper.children[1].firstChild!);
+
+        await waitFor(() => {
+            expect(mockFn).toBeCalledTimes(2);
+            expect(mockFn.mock.calls[0][0]).toBe(tabData.id);
+        });
+
+        fireEvent.mouseOut(wrapper);
+        fireEvent.click(wrapper.children[0].firstChild!);
+        mockFn.mockClear();
+        await waitFor(() => {
+            expect(mockFn).not.toBeCalled();
+        });
+    });
+
+    test('Should trigger contextmenu event', () => {
+        const mockFn = jest.fn();
+        const { container } = render(<DTab onContextMenu={mockFn} />);
+        const wrapper = container.querySelector(`.${tabItemClassName}`)!;
+
+        fireEvent.contextMenu(wrapper);
+
+        expect(mockFn).toBeCalled();
+    });
+
+    test('Should render support to sort via drag and drop', () => {
+        const mockFn = jest.fn();
+        const { container } = render(
+            <DragAndDrop>
+                <Tab onMoveTab={mockFn} id="1" name="test1" index={1} />
+                <Tab onMoveTab={mockFn} id="2" name="test2" index={2} />
+                <Tab onMoveTab={mockFn} id="3" name="test3" index={3} />
+            </DragAndDrop>
+        );
+
+        const tabs = container.querySelectorAll<HTMLDivElement>(
+            `.${tabItemClassName}`
+        )!;
+
+        // Same source and target will do nothing;
+        dragToTargetNode(tabs[1], tabs[1]);
+        expect(mockFn).not.toBeCalled();
+
+        // normal
+        dragToTargetNode(tabs[1], tabs[0]);
+        expect(mockFn).toBeCalled();
+        expect(mockFn.mock.calls[0][0]).toBe(2);
+        expect(mockFn.mock.calls[0][1]).toBe(1);
+    });
+
+    test('Should do nothing when drag up', () => {
+        const originalBoundingClientRect =
+            HTMLDivElement.prototype.getBoundingClientRect;
+        //@ts-ignore
+        const originalClientX = Event.prototype.clientX;
+        //@ts-ignore
+        const originalClientY = Event.prototype.clientY;
+
+        const mockFn = jest.fn();
+        const { container } = render(
+            <DragAndDrop>
+                <Tab onMoveTab={mockFn} id="1" name="test1" index={1} />
+                <Tab onMoveTab={mockFn} id="2" name="test2" index={2} />
+                <Tab onMoveTab={mockFn} id="3" name="test3" index={3} />
+            </DragAndDrop>
+        );
+
+        const tabs = container.querySelectorAll<HTMLDivElement>(
+            `.${tabItemClassName}`
+        )!;
+
+        mockClientOffset(5, 10);
+        mockFn.mockClear();
+        dragToTargetNode(tabs[1], tabs[0]);
+        expect(mockFn).not.toBeCalled();
+
+        // reset mock function
+        HTMLDivElement.prototype.getBoundingClientRect = originalBoundingClientRect;
+        // @ts-ignore
+        Event.prototype.clientX = originalClientX;
+        // @ts-ignore
+        Event.prototype.clientY = originalClientY;
+    });
+
+    test('Should do nothing when drag down', () => {
+        const originalBoundingClientRect =
+            HTMLDivElement.prototype.getBoundingClientRect;
+        //@ts-ignore
+        const originalClientX = Event.prototype.clientX;
+        //@ts-ignore
+        const originalClientY = Event.prototype.clientY;
+
+        const mockFn = jest.fn();
+        const { container } = render(
+            <DragAndDrop>
+                <Tab onMoveTab={mockFn} id="1" name="test1" index={1} />
+                <Tab onMoveTab={mockFn} id="2" name="test2" index={2} />
+                <Tab onMoveTab={mockFn} id="3" name="test3" index={3} />
+            </DragAndDrop>
+        );
+
+        const tabs = container.querySelectorAll<HTMLDivElement>(
+            `.${tabItemClassName}`
+        )!;
+
+        mockClientOffset(20, 10);
+        mockFn.mockClear();
+        dragToTargetNode(tabs[0], tabs[1]);
+        expect(mockFn).not.toBeCalled();
+
+        // reset mock function
+        HTMLDivElement.prototype.getBoundingClientRect = originalBoundingClientRect;
+        // @ts-ignore
+        Event.prototype.clientX = originalClientX;
+        // @ts-ignore
+        Event.prototype.clientY = originalClientY;
+    });
+});
---
+++ src/components/tabs/__tests__/tabs.test.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import { Tabs, tabsClassName, tabsContentActiveClassName } from '..';
+import { ITabProps, tabItemActiveClassName } from '../tab';
+import { dragToTargetNode } from './tab.test';
+
+const mockData: ITabProps[] = [
+    {
+        id: '1',
+        name: 'test-title-1',
+        renderPane: <div data-testid="test1">test1</div>,
+        closable: true,
+    },
+    {
+        id: '2',
+        name: 'test-title-2',
+        renderPane: <div data-testid="test2">test2</div>,
+        closable: true,
+    },
+];
+
+afterEach(cleanup);
+
+describe('The Tabs Components', () => {
+    test('The snapshot', () => {
+        const component = renderer.create(<Tabs data={mockData} />);
+        const tabs = component.toJSON();
+        expect(tabs).toMatchSnapshot();
+    });
+
+    test('Should render nothing', () => {
+        const { container } = render(<Tabs />);
+        const content = container.querySelector('.mo-tabs__content');
+        const header = container.querySelector('.mo-tabs__header');
+
+        expect(content?.childElementCount).toBe(0);
+        expect(header?.childElementCount).toBe(0);
+    });
+
+    test('Default type is line', () => {
+        const { container } = render(
+            <Tabs data={mockData} className="test-tabs" />
+        );
+
+        const body = container.querySelector(`.${tabsClassName}`)!;
+        expect(body.classList).toContain('mo-tabs--line');
+        expect(body.classList).toContain('test-tabs');
+    });
+
+    test('Should support to renderPane via function', () => {
+        const data = mockData.concat();
+        data.push({
+            id: '3',
+            name: 'test-title-3',
+            renderPane: () => <div data-testid="test3">test3</div>,
+            closable: true,
+        });
+        const { getByTestId } = render(<Tabs data={data} />);
+        expect(getByTestId('test3')).toBeInTheDocument();
+    });
+
+    test('Should render card type', () => {
+        const { container } = render(<Tabs type="card" data={mockData} />);
+
+        const body = container.querySelector(`.${tabsClassName}`)!;
+        expect(body.classList).toContain('mo-tabs--card');
+    });
+
+    test('Should active specific tab', () => {
+        const { getByText, getByTestId } = render(
+            <Tabs activeTab="1" data={mockData} />
+        );
+        const testHeader = getByText('test-title-1');
+        const testContent = getByTestId('test1').parentElement;
+        expect(testHeader?.classList).toContain(tabItemActiveClassName);
+        expect(testContent?.classList).toContain(tabsContentActiveClassName);
+    });
+
+    test('Should trigger events', async () => {
+        const mockSelectTabFn = jest.fn();
+        const mockContextFn = jest.fn();
+        const mockCloseFn = jest.fn();
+        const mockMoveFn = jest.fn();
+        const { getByText } = render(
+            <Tabs
+                activeTab="1"
+                data={mockData}
+                onSelectTab={mockSelectTabFn}
+                onContextMenu={mockContextFn}
+                onCloseTab={mockCloseFn}
+                onMoveTab={mockMoveFn}
+            />
+        );
+
+        const testHeader = getByText('test-title-1');
+        fireEvent.click(testHeader);
+
+        expect(mockSelectTabFn).toBeCalled();
+        expect(mockSelectTabFn.mock.calls[0][0]).toBe('1');
+
+        fireEvent.contextMenu(testHeader);
+        expect(mockContextFn).toBeCalled();
+
+        fireEvent.mouseOver(testHeader.querySelector('.mo-tab__item__op')!);
+        await waitFor(() => {
+            fireEvent.click(testHeader.querySelector('.codicon-close')!);
+        });
+
+        expect(mockCloseFn).toBeCalled();
+        expect(mockCloseFn.mock.calls[0][0]).toBe('1');
+
+        dragToTargetNode(getByText('test-title-2'), getByText('test-title-1'));
+        expect(mockMoveFn).toBeCalled();
+        expect(mockMoveFn.mock.calls[0][0]).toEqual([
+            {
+                id: '2',
+                name: 'test-title-2',
+                renderPane: <div data-testid="test2">test2</div>,
+                closable: true,
+            },
+            {
+                id: '1',
+                name: 'test-title-1',
+                renderPane: <div data-testid="test1">test1</div>,
+                closable: true,
+            },
+        ]);
+    });
+});

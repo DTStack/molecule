7a8e38878feda368ef25cb6976c04c5bdd9db734
ProfBramble <1207107376@qq.com>
2021-08-26 13:52:52 +0800

    refactor: draw off menu unit test to independent component

--- src/components/menu/__tests__/menu.test.tsx
+++ src/components/menu/__tests__/menu.test.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { fireEvent, render, waitFor, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 
-import { Menu, MenuItem, SubMenu } from '../';
+import { Menu } from '../';
 import { Divider } from '../divider';
 import { MenuMode } from '../subMenu';
 
 import {
     activeClassName,
-    disabledClassName,
-    labelClassName,
     horizontalMenuClassName,
     verticalMenuClassName,
 } from '../base';
@@ -119,7 +117,6 @@ const menuData = [
     },
 ];
 const TEST_ID = 'test-id';
-const MOCK_JSX = <div data-testid={TEST_ID}>molecule</div>;
 
 describe('Test the Menu Component', () => {
     test('Match the List snapshot', () => {
@@ -307,135 +304,6 @@ describe('Test the Menu Component', () => {
             await waitFor(() => {
                 expect(component.classList).not.toContain(activeClassName);
             });
-        });
-    });
-});
-
-describe('Test the MenuItem Component', () => {
-    test('Set Icon to MenuItem', () => {
-        render(<MenuItem icon="warning" />);
-        const component = document.body.querySelector('.codicon-warning');
-
-        expect(component).not.toBeNull();
-    });
-
-    test('Set JSX Icon to MenuItem', () => {
-        const wrapper = render(<MenuItem icon={MOCK_JSX} />);
-        const jsx = wrapper.getByTestId(TEST_ID);
-
-        expect(jsx).not.toBeNull();
-    });
-
-    test('Set name to MenuItem', () => {
-        render(<MenuItem name={TEST_ID} />);
-        const span = document.querySelector(`.${labelClassName}`);
-
-        expect(span).not.toBeNull();
-    });
-
-    test('Set title to MenuItem', () => {
-        const wrapper = render(<MenuItem title={TEST_ID} />);
-        const jsx = wrapper.getByTitle(TEST_ID);
-
-        expect(jsx).not.toBeNull();
-    });
-
-    test('Click to MenuItem', () => {
-        const TEST_FN = jest.fn();
-        const wrapper = render(
-            <MenuItem data-testid={TEST_ID} onClick={TEST_FN} />
-        );
-        const component = wrapper.getByTestId(TEST_ID);
-
-        fireEvent.click(component);
-        expect(TEST_FN).toBeCalled();
-    });
-
-    test('Set disabled to MenuItem', () => {
-        const TEST_FN = jest.fn();
-        const wrapper = render(
-            <MenuItem data-testid={TEST_ID} disabled onClick={TEST_FN} />
-        );
-        const component = wrapper.getByTestId(TEST_ID);
-
-        fireEvent.click(component);
-
-        expect(component.classList).toContain(disabledClassName);
-    });
-
-    test('Set keybinding to MenuItem', () => {
-        const keybinding = '⇧⌘P';
-        const wrapper = render(
-            <MenuItem data-testid={TEST_ID} keybinding={keybinding} />
-        );
-        const span = wrapper
-            .getByTestId(TEST_ID)
-            .querySelectorAll<HTMLSpanElement>('span')[1];
-
-        expect(span.textContent).toBe(keybinding);
-    });
-
-    test('Set children to MenuItem', () => {
-        const wrapper = render(<MenuItem>{MOCK_JSX}</MenuItem>);
-        const component = wrapper.getByTestId(TEST_ID);
-
-        expect(component).not.toBeNull();
-    });
-
-    test('Set render to MenuItem', () => {
-        const customRender = () => MOCK_JSX;
-        const wrapper = render(<MenuItem render={customRender} />);
-        const component = wrapper.getByTestId(TEST_ID);
-
-        expect(component).not.toBeNull();
-    });
-});
-
-describe('Test the SubMenu Component', () => {
-    test('Set Icon to MenuItem', () => {
-        render(<SubMenu icon="warning" />);
-        const component = document.body.querySelector('.codicon-warning');
-
-        expect(component).not.toBeNull();
-    });
-
-    test('Set JSX Icon to MenuItem', () => {
-        const wrapper = render(<SubMenu icon={MOCK_JSX} />);
-        const jsx = wrapper.getByTestId(TEST_ID);
-
-        expect(jsx).not.toBeNull();
-    });
-
-    test('Set children to MenuItem', async () => {
-        const wrapper = render(
-            <SubMenu>
-                <MenuItem>subMenuItem1</MenuItem>
-                <MenuItem>subMenuItem2</MenuItem>
-            </SubMenu>
-        );
-        await waitFor(() => {
-            const ul = wrapper.container.firstElementChild?.children[1];
-
-            expect(ul!.children.length).toBe(2);
-        });
-    });
-
-    test('Set data to MenuItem', async () => {
-        const mockData = [
-            {
-                id: 'New File',
-                name: 'New File',
-            },
-            {
-                id: 'OpenFile',
-                name: 'Open',
-            },
-        ];
-        const wrapper = render(<SubMenu data={mockData}></SubMenu>);
-        await waitFor(() => {
-            const ul = wrapper.container.firstElementChild?.children[1];
-
-            expect(ul!.children.length).toBe(mockData.length);
         });
     });
 });

1fc0485b9ecdecd388072462dc7c3a7295fa00d9
xiaowei <wewoor@gmail.com>
2021-08-30 14:59:51 +0800

    refactor: rename the closeOthers to closeOther

--- src/model/workbench/editor.ts
+++ src/model/workbench/editor.ts
@@ -8,7 +8,7 @@ import { editor as MonacoEditor } from 'monaco-editor';
 export enum EditorEvent {
     OnCloseTab = 'editor.closeTab',
     OnCloseAll = 'editor.closeAll',
-    OnCloseOthers = 'editor.closeOthers',
+    OnCloseOther = 'editor.closeOther',
     OnCloseToLeft = 'editor.closeToLeft',
     OnCloseToRight = 'editor.closeToRight',
     OnMoveTab = 'editor.moveTab',
@@ -64,7 +64,7 @@ export interface IEditor {
     /**
      * Current editor group
      */
-    current?: IEditorGroup | null;
+    current?: IEditorGroup;
     /**
      * Editor Groups
      */
@@ -162,13 +162,13 @@ export class EditorGroupModel<E = any, T = any> implements IEditorGroup<E, T> {
 }
 
 export class EditorModel implements IEditor {
-    public current: IEditorGroup | null;
+    public current: IEditorGroup | undefined;
     public groups: IEditorGroup[];
     public entry: React.ReactNode;
     public editorOptions: IEditorOptions;
 
     constructor(
-        current: IEditorGroup | null = null,
+        current: IEditorGroup | undefined = undefined,
         groups: IEditorGroup[] = [],
         entry: React.ReactNode,
         editorOptions: IEditorOptions = BuiltInEditorOptions

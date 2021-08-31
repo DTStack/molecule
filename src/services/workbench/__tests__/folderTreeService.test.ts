708e1d556b60e9061d9e69ae8efa13fca4f45b0f
ProfBramble <1207107376@qq.com>
2021-08-31 19:54:33 +0800

    test: add folder and file tree unit test

--- src/services/workbench/__tests__/folderTreeService.test.ts
+++ src/services/workbench/__tests__/folderTreeService.test.ts
@@ -1,9 +1,16 @@
+import { Button } from 'mo/components/button';
 import 'reflect-metadata';
 import { container } from 'tsyringe';
+import { FileTypes } from 'mo/model/workbench/explorer/folderTree';
+import { expectFnCalled } from '@test/utils';
+
 import {
     IFolderTreeService,
     FolderTreeService,
 } from '../explorer/folderTreeService';
+import { FolderTreeEvent } from 'mo/model/workbench/explorer/folderTree';
+
+const TEST_ID = 'test-id';
 
 describe('Test StatusBarService', () => {
     const folderTreeService = container.resolve<IFolderTreeService>(
@@ -26,9 +33,12 @@ describe('Test StatusBarService', () => {
         },
     ];
 
+    beforeEach(() => {
+        folderTreeService.reset();
+    });
+
     test('FolderTreeService Class instance', () => {
         expect(folderTreeService).not.toBeUndefined();
-        console.log(folderTreeService.getState());
         expect(folderTreeService.getState()).not.toBeUndefined();
     });
 
@@ -50,5 +60,119 @@ describe('Test StatusBarService', () => {
     test('Should support to set folder context menu', () => {
         folderTreeService.setFolderContextMenu(mockMenuProps);
         expect(folderTreeService.getFolderContextMenu()).toEqual(mockMenuProps);
+    });
+
+    test('Should support to set root folder tree', () => {
+        const mockRootTree = {
+            id: '0',
+            name: 'test0',
+            fileType: 'RootFolder',
+            location: 'test0',
+        };
+        const mockTreeData = {
+            id: '1',
+            key: '1',
+            name: 'test1',
+            children: [
+                {
+                    id: '2',
+                    key: '1-1',
+                    name: 'test1-1',
+                    icon: 'test',
+                    isLeaf: true,
+                    fileType: FileTypes.File,
+                },
+            ],
+        };
+        const mockFileData = {
+            id: '3',
+            key: '1-2',
+            name: 'test1-2',
+            icon: 'test',
+            isLeaf: true,
+            fileType: FileTypes.File,
+        };
+
+        folderTreeService.add(mockRootTree);
+        expect(folderTreeService.get(0)).toEqual(mockRootTree);
+
+        folderTreeService.add(mockTreeData, 0);
+        expect(folderTreeService.get(1)).toEqual(mockTreeData);
+
+        folderTreeService.add(mockFileData, 2);
+        expect(folderTreeService.get(3)).toEqual(mockFileData);
+
+        folderTreeService.setActive(0);
+        const currentNode = folderTreeService.getState()?.folderTree!.current;
+        expect(currentNode!.id).toBe('0');
+
+        folderTreeService.remove(2);
+        expect(folderTreeService.get(2)).toBeNull();
+
+        mockTreeData.name = TEST_ID;
+        folderTreeService.update(mockTreeData);
+        expect(folderTreeService.get(1)?.name).toBe(TEST_ID);
+    });
+
+    test('Should support to set entry', () => {
+        folderTreeService.setEntry(Button);
+        expect(folderTreeService.getState().entry).toEqual(Button);
+    });
+
+    test('Should support to rename', () => {
+        expectFnCalled((fn) => {
+            folderTreeService.onRename(fn);
+            folderTreeService.emit(FolderTreeEvent.onRename);
+        });
+    });
+
+    test('Should support to remove', () => {
+        expectFnCalled((fn) => {
+            folderTreeService.onRemove(fn);
+            folderTreeService.emit(FolderTreeEvent.onDelete);
+        });
+    });
+
+    test('Should support to update file name', () => {
+        expectFnCalled((fn) => {
+            folderTreeService.onUpdateFileName(fn);
+            folderTreeService.emit(FolderTreeEvent.onUpdateFileName, 0);
+            return 0;
+        });
+    });
+
+    test('Should support to select file', () => {
+        expectFnCalled((fn) => {
+            folderTreeService.onSelectFile(fn);
+            folderTreeService.emit(FolderTreeEvent.onSelectFile);
+        });
+    });
+
+    test('Should support to right click', () => {
+        expectFnCalled((fn) => {
+            folderTreeService.onRightClick(fn);
+            folderTreeService.emit(FolderTreeEvent.onRightClick);
+        });
+    });
+
+    test('Should support to right click', () => {
+        expectFnCalled((fn) => {
+            folderTreeService.onCreate(fn);
+            folderTreeService.emit(FolderTreeEvent.onCreate);
+        });
+    });
+
+    test('Should support to right click', () => {
+        expectFnCalled((fn) => {
+            folderTreeService.onContextMenu(fn);
+            folderTreeService.emit(FolderTreeEvent.onContextMenuClick);
+        });
+    });
+
+    test('Should support to load data', () => {
+        expectFnCalled((fn) => {
+            folderTreeService.onLoadData(fn);
+            folderTreeService.emit(FolderTreeEvent.onLoadData);
+        });
     });
 });

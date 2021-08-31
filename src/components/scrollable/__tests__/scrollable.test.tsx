f67369f79471a7b6e2d18aa2c5228eb678686924
xiaowei <wewoor@gmail.com>
2021-08-31 20:05:03 +0800

    test: new cases for scrolling and mouse events

--- src/components/scrollable/__tests__/scrollable.test.tsx
+++ src/components/scrollable/__tests__/scrollable.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import { IScrollbarProps, Scrollable } from '../index';
@@ -8,14 +8,24 @@ function TestScrollable(props: IScrollbarProps) {
     return (
         <div>
             <Scrollable {...(props as any)}>
-                {Array(100)
-                    .fill('')
-                    .map((_, index) => {
-                        return <div key={index}>{index}</div>;
-                    })}
+                <div data-testid="mytest">
+                    {Array(100)
+                        .fill('')
+                        .map((_, index) => {
+                            return <div key={index}>{index}</div>;
+                        })}
+                </div>
             </Scrollable>
         </div>
     );
+}
+
+function thumbElement(container: HTMLElement): HTMLSpanElement {
+    return container.querySelector<HTMLSpanElement>('.ScrollbarsCustom-Thumb')!;
+}
+
+function trackElement(container: HTMLElement): HTMLSpanElement {
+    return container.querySelector<HTMLSpanElement>('.ScrollbarsCustom-Track')!;
 }
 
 describe('Test Scrollable Component', () => {
@@ -27,52 +37,58 @@ describe('Test Scrollable Component', () => {
     test('Show shadow style', () => {
         const { container, rerender } = render(<TestScrollable />);
         expect(container.querySelector('.shadow')).toBeNull();
-        rerender(<TestScrollable isShowShadow />);
 
+        rerender(<TestScrollable isShowShadow />);
         expect(container.querySelector('.shadow')).not.toBeNull();
 
         rerender(<TestScrollable isShowShadow={false} />);
         expect(container.querySelector('.shadow')).toBeNull();
-
-        rerender(<TestScrollable isShowShadow={true} />);
-        expect(container.querySelector('.shadow')).not.toBeNull();
     });
 
     test('Custom the Track style', () => {
         const { container, rerender } = render(<TestScrollable />);
-        expect(
-            container.querySelector<HTMLSpanElement>('.ScrollbarsCustom-Track')
-                ?.style?.width
-        ).toBe('10px');
+        expect(trackElement(container).style.width).toBe('10px');
 
         rerender(<TestScrollable trackStyle={{ width: '12px' }} />);
-        expect(
-            container.querySelector<HTMLSpanElement>('.ScrollbarsCustom-Track')
-                ?.style?.width
-        ).toBe('12px');
+        expect(trackElement(container).style.width).toBe('12px');
     });
 
     test('Custom the className', () => {
         const { container, rerender } = render(<TestScrollable />);
-        expect(container.querySelector<HTMLSpanElement>('.myclass')).toBeNull();
+        expect(
+            container.querySelector<HTMLSpanElement>('.myclass')
+        ).not.toBeInTheDocument();
 
         rerender(<TestScrollable className="myclass" />);
         expect(
             container.querySelector<HTMLSpanElement>('.myclass')
-        ).not.toBeNull();
+        ).toBeInTheDocument();
     });
 
     test('Custom the Thumb style', () => {
         const { container, rerender } = render(<TestScrollable />);
-        expect(
-            container.querySelector<HTMLSpanElement>('.ScrollbarsCustom-Thumb')
-                ?.style?.width
-        ).toBe('100%');
+        expect(thumbElement(container).style.width).toBe('100%');
 
         rerender(<TestScrollable thumbStyle={{ width: '12px' }} />);
-        expect(
-            container.querySelector<HTMLSpanElement>('.ScrollbarsCustom-Thumb')
-                ?.style?.width
-        ).toBe('12px');
+        expect(thumbElement(container).style.width).toBe('12px');
+    });
+
+    test('Mouse move on the scrollable content', () => {
+        const { container, getByTestId } = render(<TestScrollable />);
+        const rootEle = getByTestId('mytest');
+        fireEvent.mouseEnter(rootEle);
+        expect(trackElement(container).style.opacity).toBe('1');
+
+        fireEvent.mouseLeave(rootEle);
+        expect(trackElement(container).style?.opacity).toBe('0');
+    });
+
+    test('Scrolling the content', async () => {
+        const { container, getByTestId } = render(
+            <TestScrollable isShowShadow />
+        );
+        const rootEle = getByTestId('mytest');
+        fireEvent.scroll(rootEle);
+        expect(trackElement(container).style.opacity).toBe('1');
     });
 });

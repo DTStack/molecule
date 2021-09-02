59fcd3c48f50b4a1c482af085ff0ae5d6e8a3386
xiaowei <wewoor@gmail.com>
2021-09-02 16:26:06 +0800

    style: prettify the code

--- test/setupTests.tsx
+++ test/setupTests.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 jest.mock('mo/monaco/monacoService', () => {
-    function MonacoService() { }
-    const getter = { get: () => { } };
+    function MonacoService() {}
+    const getter = { get: () => {} };
 
-    MonacoService.prototype.create = function (dom, options, override) { };
+    MonacoService.prototype.create = function (dom, options, override) {};
     MonacoService.prototype.initWorkspace = function (
         container: HTMLElement
-    ) { };
+    ) {};
     MonacoService.prototype.services = getter;
     MonacoService.prototype.commandService = getter;
     MonacoService.prototype.container = getter;

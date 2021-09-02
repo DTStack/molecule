6bb70434a9e0e6b8419893f1b136c77c5a8b8e05
xiaowei <wewoor@gmail.com>
2021-09-02 16:22:44 +0800

    refactor: remove the useless code

--- src/workbench/__tests__/workbench.test.tsx
+++ src/workbench/__tests__/workbench.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import { WorkbenchView, Workbench } from '../workbench';

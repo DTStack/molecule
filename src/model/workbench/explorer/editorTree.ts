d43dee493ce1a3a8a6325565e47f9530c37484d8
野迂迂 <yangwei1@outlook.com>
2021-09-03 11:52:13 +0800

    test: add unit test for editorTree in workbench (#386)
    * test: add unit test for editorTree in workbench
    
    * feat: editorTree support to toolbarClick event
    
    * refactor: improve editorTree for testing
    
    * test: add test for editorTree
    
    * test: improve test
    
    * test: replace the scrollable mock

--- src/model/workbench/explorer/editorTree.ts
+++ src/model/workbench/explorer/editorTree.ts
@@ -14,6 +14,7 @@ export enum EditorTreeEvent {
     onCloseAll = 'editorTree.closeAll',
     onSaveAll = 'editorTree.saveAll',
     onSplitEditorLayout = 'editorTree.splitEditorLayout',
+    onToolbarClick = 'editorTree.toolbarClick',
     onContextMenu = 'editorTree.contextMenuClick',
 }
 

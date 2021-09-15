38d16d669c558918453d52a4448a65db3da12c9d
xiaowei <wewoor@gmail.com>
2021-09-15 23:49:21 +0800

    feat: collapse the folderTree

--- src/extensions/explorer/index.ts
+++ src/extensions/explorer/index.ts
@@ -1,10 +1,21 @@
 import molecule from 'mo';
+import { COLLAPSE_FOLDER_COMMAND_ID, SAMPLE_FOLDER_PANEL_ID } from 'mo/model';
 import { IExtension } from 'mo/model/extension';
 
 export const ExtendsExplorer: IExtension = {
     activate() {
         molecule.explorer.onRemovePanel((panel) => {
             molecule.explorer.removePanel(panel.id);
+        });
+
+        // Implements to collapse the expanded tree nodes
+        molecule.explorer.onPanelToolbarClick((panel, actionId) => {
+            if (
+                panel.id === SAMPLE_FOLDER_PANEL_ID &&
+                actionId === COLLAPSE_FOLDER_COMMAND_ID
+            ) {
+                molecule.folderTree.setExpandedKeys([]);
+            }
         });
     },
 };

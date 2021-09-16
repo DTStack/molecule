bcc0987b9ede9c7d6eb2918404c1c706bd26edcd
ProfBramble <1207107376@qq.com>
2021-09-16 16:01:46 +0800

    test: add remove and removeContextMenu unit tests

--- src/services/__tests__/activityBarService.test.ts
+++ src/services/__tests__/activityBarService.test.ts
@@ -7,14 +7,20 @@ import { expectLoggerErrorToBeCalled } from '@test/utils';
 
 const activityBarService = container.resolve(ActivityBarService);
 
-const mockBarData: IActivityBarItem = {
-    id: '1',
-    name: 'test',
-};
+const mockBarData: IActivityBarItem[] = [
+    {
+        id: '1',
+        name: 'test1',
+    },
+    { id: '2', name: 'test2' },
+];
 
-const mockMenuData: IMenuItemProps = {
-    id: '1',
-};
+const mockMenuData: IMenuItemProps[] = [
+    {
+        id: '1',
+    },
+    { id: '2' },
+];
 
 describe('The ActivityBar Services', () => {
     afterEach(() => {
@@ -22,22 +28,22 @@ describe('The ActivityBar Services', () => {
     });
 
     test('Should support to add a single bar data', () => {
-        activityBarService.add(mockBarData);
+        activityBarService.add(mockBarData[0]);
 
         const { data } = activityBarService.getState();
         expect(data).toHaveLength(1);
-        expect(data![0]).toEqual(mockBarData);
+        expect(data![0]).toEqual(mockBarData[0]);
     });
 
     test('Should support to add a single bar data meanwhile active it', () => {
-        activityBarService.add(mockBarData, true);
+        activityBarService.add(mockBarData[0], true);
 
         const { selected } = activityBarService.getState();
         expect(selected).toBe('1');
     });
 
     test('Should support to batch add bars', () => {
-        const batchData = [mockBarData, { id: '2', name: 'test' }];
+        const batchData = [mockBarData[0], { id: '2', name: 'test' }];
         activityBarService.add(batchData);
 
         const { data } = activityBarService.getState();
@@ -46,11 +52,21 @@ describe('The ActivityBar Services', () => {
 
     test('Should support remove bar data', () => {
         const state = activityBarService.getState();
-        activityBarService.add(mockBarData);
+        activityBarService.add(mockBarData[0]);
 
         expect(state.data).toHaveLength(1);
 
         activityBarService.remove('1');
+        expect(state.data).toHaveLength(0);
+    });
+
+    test('Should support remove bar array data', () => {
+        const state = activityBarService.getState();
+        activityBarService.add(mockBarData);
+
+        expect(state.data).toHaveLength(2);
+
+        activityBarService.remove(['1', '2']);
         expect(state.data).toHaveLength(0);
     });
 
@@ -62,7 +78,7 @@ describe('The ActivityBar Services', () => {
 
     test('Should support to hidden acitivity bar', () => {
         const state = activityBarService.getState();
-        activityBarService.add(mockBarData);
+        activityBarService.add(mockBarData[0]);
         expect(state.data![0].hidden).toBeFalsy();
 
         activityBarService.toggleBar('1');
@@ -77,7 +93,7 @@ describe('The ActivityBar Services', () => {
 
     test('Should select the following one if current bar is selected', () => {
         const state = activityBarService.getState();
-        activityBarService.add(mockBarData, true);
+        activityBarService.add(mockBarData[0], true);
         activityBarService.add({ id: '2', name: 'test' });
 
         expect(state.selected).toBe('1');
@@ -88,7 +104,7 @@ describe('The ActivityBar Services', () => {
 
     test('Should select the first one if current bar is selected and is last one', () => {
         const state = activityBarService.getState();
-        activityBarService.add(mockBarData);
+        activityBarService.add(mockBarData[0]);
         activityBarService.add({ id: '2', name: 'test' }, true);
 
         expect(state.selected).toBe('2');
@@ -99,26 +115,26 @@ describe('The ActivityBar Services', () => {
 
     test('Should support to add a context menu', () => {
         const state = activityBarService.getState();
-        activityBarService.addContextMenu(mockMenuData);
+        activityBarService.addContextMenu(mockMenuData[0]);
 
         expect(state.contextMenu).toHaveLength(1);
-        expect(state.contextMenu![0]).toEqual(mockMenuData);
+        expect(state.contextMenu![0]).toEqual(mockMenuData[0]);
     });
 
     test('Should support to batch add context menus', () => {
         const state = activityBarService.getState();
-        const menus = [mockMenuData, { id: '2' }];
+        const menus = [mockMenuData[0], { id: '2' }];
         activityBarService.addContextMenu(menus);
 
         expect(state.contextMenu).toHaveLength(menus.length);
-        expect(state.contextMenu![0]).toEqual(mockMenuData);
+        expect(state.contextMenu![0]).toEqual(mockMenuData[0]);
         expect(state.contextMenu![1]).toEqual(menus[1]);
     });
 
     test('Should support to toggle the status of context menus', () => {
         const state = activityBarService.getState();
 
-        activityBarService.addContextMenu(mockMenuData);
+        activityBarService.addContextMenu(mockMenuData[0]);
         expect(state.contextMenu![0].icon).toBeFalsy();
 
         activityBarService.toggleContextMenuChecked('1');
@@ -133,9 +149,17 @@ describe('The ActivityBar Services', () => {
 
     test('Should support to remove context menus', () => {
         const state = activityBarService.getState();
-        activityBarService.addContextMenu(mockMenuData);
+        activityBarService.addContextMenu(mockMenuData[0]);
 
         activityBarService.removeContextMenu('1');
+        expect(state.contextMenu).toHaveLength(0);
+    });
+
+    test('Should support to remove context menus array', () => {
+        const state = activityBarService.getState();
+        activityBarService.addContextMenu(mockMenuData);
+
+        activityBarService.removeContextMenu(['1', '2']);
         expect(state.contextMenu).toHaveLength(0);
     });
 
@@ -151,13 +175,13 @@ describe('The ActivityBar Services', () => {
 
         activityBarService.emit(
             ActivityBarEvent.OnClick,
-            mockBarData.id,
-            mockBarData
+            mockBarData[0].id,
+            mockBarData[0]
         );
 
         expect(mockFn).toBeCalled();
-        expect(mockFn.mock.calls[0][0]).toBe(mockBarData.id);
-        expect(mockFn.mock.calls[0][1]).toBe(mockBarData);
+        expect(mockFn.mock.calls[0][0]).toBe(mockBarData[0].id);
+        expect(mockFn.mock.calls[0][1]).toBe(mockBarData[0]);
     });
 
     test('Should support to subscribe onChange', () => {

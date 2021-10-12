c3ef55c60edbbf97fb62b2b762e2d843c89969ec
ProfBramble <1207107376@qq.com>
2021-10-12 19:52:52 +0800

    feat: add localeService adaptation and judgment

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,3 +1,3 @@
 export { LocaleService } from './localeService';
 export type { ILocaleService } from './localeService';
-export type { ILocale } from './localization';
+export type { ILocale, ILocales } from './localization';

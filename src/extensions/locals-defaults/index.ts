b000e7ff8d7cc048bae1f21c1a1b5bd89a0f3648
ProfBramble <1207107376@qq.com>
2021-10-12 19:44:50 +0800

    feat: add locals default extensions

---
+++ src/extensions/locals-defaults/index.ts
@@ -0,0 +1,21 @@
+import { IExtension, IContributeType } from 'mo/model/extension';
+const en = require('./locals/en.json');
+const defaultId = en.id;
+const zhCN = require('./locals/zh-CN.json');
+
+export const ExtendsLocals: IExtension = {
+    contributes: {
+        [IContributeType.Languages]: {
+            localeId: defaultId,
+            locales: [en, zhCN],
+        },
+    },
+    activate() {}, // todo
+};
+
+const builtInConfig = ExtendsLocals!.contributes?.[IContributeType.Languages];
+
+export const BuiltInLocales = builtInConfig!.locales;
+export const BuiltInDefault = builtInConfig!.locales.find(
+    (item) => item.id === builtInConfig!.localeId
+);

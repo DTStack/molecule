4d79c7994a7665507e08c55d1aebd9566f2afb68
ProfBramble <1207107376@qq.com>
2021-10-13 15:14:47 +0800

    feat: specify language packages using package

--- src/extensions/locals-defaults/index.ts
+++ src/extensions/locals-defaults/index.ts
@@ -1,21 +1,27 @@
 import { IExtension, IContributeType } from 'mo/model/extension';
-const en = require('./locals/en.json');
-const defaultId = en.id;
-const zhCN = require('./locals/zh-CN.json');
+const defaultLocalsExtension = require('./package.json');
+
+interface ILocaleList {
+    contributes: {
+        languages: { id: string; path: string }[];
+    };
+}
+
+const getLocalesList = (objs: ILocaleList) => {
+    return objs.contributes.languages.map((obj) => {
+        const value = require(obj.path);
+        return value;
+    });
+};
+
+const locales = getLocalesList(defaultLocalsExtension) || [];
 
 export const ExtendsLocals: IExtension = {
     contributes: {
-        [IContributeType.Languages]: {
-            localeId: defaultId,
-            locales: [en, zhCN],
-        },
+        [IContributeType.Languages]: locales,
     },
     activate() {},
 };
 
-const builtInConfig = ExtendsLocals!.contributes?.[IContributeType.Languages];
-
-export const BuiltInLocales = builtInConfig!.locales;
-export const BuiltInDefault = builtInConfig!.locales.find(
-    (item) => item.id === builtInConfig!.localeId
-);
+export const BuiltInLocales = locales;
+export const BuiltInDefault = locales.find((item) => item.id === 'en');

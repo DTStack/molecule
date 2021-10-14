8619a2be04c80a14a09835efa66c17ff486bed9e
ProfBramble <1207107376@qq.com>
2021-10-14 16:42:55 +0800

    feat: redefine locales interface

--- src/model/locales.ts
+++ src/model/locales.ts
@@ -1,7 +1,7 @@
+import { LocaleSourceIdType } from 'mo/i18n/localization';
+
 export interface ILocales {
-    /**
-     * The id of component, locale will be applied by this ID
-     */
     id: string;
-    path: string;
+    name: string;
+    source: LocaleSourceIdType;
 }

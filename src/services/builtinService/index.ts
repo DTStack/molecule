3b6893aa609512ee82cb536d1961667ad98d1b30
mortalYoung <yangwei1@outlook.com>
2021-10-19 19:39:12 +0800

    feat: add the annotations for built-in service

--- src/services/builtinService/index.ts
+++ src/services/builtinService/index.ts
@@ -20,14 +20,33 @@ export type IBuiltinProps = IBuiltinModuleProps & IBuiltinConstantProps;
 
 export interface IBuiltinService {
     /**
-     * @deprecated
+     * Mark the specific constant as inactive
+     * @deprecated we're considering the necessary of this method, because it's useless for now to inactive a constant
      */
     inactiveConstant(id: keyof typeof constants): boolean;
+    /**
+     * Mark the specific module as inactive
+     */
     inactiveModule(id: keyof typeof modules): boolean;
+    /**
+     * Get the specific constant by id
+     */
     getConstant(id: keyof typeof constants): IBuiltinConstantProps | undefined;
+    /**
+     * Get all constants
+     */
     getConstants(): Partial<typeof constants>;
+    /**
+     * Get the specific module by id
+     */
     getModule<T>(id: keyof typeof modules): IBuiltinModuleProps<T> | undefined;
+    /**
+     * Get all modules
+     */
     getModules(): Partial<typeof modules>;
+    /**
+     * Reset all constants and modules
+     */
     reset(): void;
 }
 

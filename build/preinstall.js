b8d9e9a877436876d1696559edd2e4f6a7b7a42d
野迂迂 <yangwei1@outlook.com>
2021-11-22 18:07:02 +0800

    build: update the limitation of node version (#515)

--- build/preinstall.js
+++ build/preinstall.js
@@ -2,14 +2,11 @@ let err = false;
 
 const majorNodeVersion = parseInt(/^(\d+)\./.exec(process.versions.node)[1]);
 
-if (majorNodeVersion < 10 || majorNodeVersion > 14) {
-    console.error('\033[1;31m*** Please use node >=10 and <=14.\033[0;0m');
+if (majorNodeVersion < 10) {
+    console.error('\033[1;31m*** Please use node >=10.\033[0;0m\n');
     err = true;
 }
 
-const cp = require('child_process');
-
 if (err) {
-    console.error('');
     process.exit(1);
 }

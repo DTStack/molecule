a0bea176f52120963b1f6a2541ebdbbc12535aea
xiaowei <wewoor@gmail.com>
2021-12-06 15:06:26 +0800

    docs: remove useless community info

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -142,25 +142,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
                         title: 'Community',
                         items: [
                             {
-                                label: 'Stack Overflow',
-                                href:
-                                    'https://stackoverflow.com/questions/tagged/docusaurus',
-                            },
-                            {
-                                label: 'Discord',
-                                href:
-                                    'https://discordapp.com/invite/docusaurus',
-                            },
-                            {
-                                label: 'Twitter',
-                                href: 'https://twitter.com/docusaurus',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            {
                                 label: 'GitHub',
                                 href: 'https://github.com/DTStack/molecule',
                             },

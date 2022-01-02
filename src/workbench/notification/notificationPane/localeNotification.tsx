a3b41703710b39a55ed6b8dcc8c9b7ae2cdb9897
Benjamin Yang <ysyang@chequer.io>
2022-01-02 22:33:46 +0900

    feat: increase the readability of small samples in the Workbench of the Storybook #591

--- src/workbench/notification/notificationPane/localeNotification.tsx
+++ src/workbench/notification/notificationPane/localeNotification.tsx
@@ -13,24 +13,30 @@ export function LocaleNotification(props: ILocaleNotificationProps) {
     }, []);
 
     return (
-        <div
-            style={{
-                lineHeight: '1.5',
-                width: 350,
-                textAlign: 'left',
-            }}
-        >
-            <p>
-                The current locale has changed to {locale}, click the button to
-                reload the Page and applying the changes.
-            </p>
-            <p style={{ fontWeight: 'bold' }}>
-                Notice: Reload the Page could lose the data, Please confirm you
-                have saved before.
-            </p>
-            <Button style={{ width: 150 }} onClick={reload}>
-                Confirm Reload
-            </Button>
+        <div>
+            <div
+                style={{
+                    lineHeight: '1.5',
+                    width: 400,
+                    textAlign: 'left',
+                }}
+            >
+                <p>
+                    The current locale has changed to {locale}, click the button
+                    to reload the Page and applying the changes.
+                </p>
+                <p style={{ fontWeight: 'bold' }}>
+                    Notice: Reload the Page could lose the data, Please confirm
+                    you have saved before.
+                </p>
+            </div>
+            <div>
+                <p style={{ direction: 'rtl' }}>
+                    <Button style={{ width: 150 }} onClick={reload}>
+                        Confirm Reload
+                    </Button>
+                </p>
+            </div>
         </div>
     );
 }

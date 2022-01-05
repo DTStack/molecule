26fa0c740e7b280037a8c0ecfcf55054f2356ea7
xiaowei <wewoor@gmail.com>
2022-01-05 15:59:34 +0800

    feat: add use case for disposing an action

---
+++ stories/extensions/actions/quickOpen.ts
@@ -0,0 +1,51 @@
+import { IQuickInputService } from 'monaco-editor/esm/vs/platform/quickinput/common/quickInput';
+import { KeyChord } from 'monaco-editor/esm/vs/base/common/keyCodes';
+
+import { debounce } from 'lodash';
+import { Action2, KeybindingWeight, KeyCode, KeyMod } from 'mo/monaco';
+
+export class QuickOpenAction extends Action2 {
+    static readonly ID = 'QuickOpenFile';
+    static readonly LABEL = 'Search files by name';
+
+    constructor() {
+        super({
+            id: QuickOpenAction.ID,
+            label: QuickOpenAction.LABEL,
+            title: QuickOpenAction.LABEL,
+            alias: QuickOpenAction.LABEL,
+            precondition: undefined,
+            f1: true, // Whether show the QuickOpenFile in Command Palette
+            keybinding: {
+                weight: KeybindingWeight.WorkbenchContrib,
+                when: undefined,
+                // eslint-disable-next-line new-cap
+                primary: KeyChord(KeyMod.CtrlCmd | KeyCode.KeyP),
+            },
+        });
+    }
+
+    run(accessor: any, ...args: any[]) {
+        const quickInputService = accessor.get(IQuickInputService);
+
+        const quickPick = quickInputService.createQuickPick();
+        quickPick.items = [];
+        quickPick.placeholder = QuickOpenAction.LABEL;
+
+        quickPick.activeItems = [];
+        quickPick.canSelectMany = false;
+
+        const queryPick = debounce((value) => {}, 300);
+
+        quickPick.onDidChangeValue(queryPick);
+
+        quickPick.onDidAccept((i: any) => {
+            const item = quickPick.activeItems[0];
+            if (item) {
+                alert('quickOpen');
+            }
+            quickPick.hide();
+        });
+        quickPick.show();
+    }
+}

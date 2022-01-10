18be37b976e4824eb850e2a084718ef603a90773
xiaowei <wewoor@gmail.com>
2022-01-10 16:07:09 +0800

    refactor: export Action2 from action.ts

--- src/monaco/api.ts
+++ src/monaco/api.ts
@@ -1,3 +1,4 @@
 export { KeyChord } from 'monaco-editor/esm/vs/base/common/keyCodes';
 export type { IQuickInputService } from 'monaco-editor/esm/vs/platform/quickinput/common/quickInput';
-export { KeybindingWeight, Action2 } from './common';
+export { KeybindingWeight } from './common';
+export { Action2 } from './action';

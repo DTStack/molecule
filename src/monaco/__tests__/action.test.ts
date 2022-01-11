7e4ca59a9608dc45b2de96a0566068e47387ae7a
xiaowei <wewoor@gmail.com>
2022-01-11 15:34:36 +0800

    test: unit tests for menu, keybinding branchs

--- src/monaco/__tests__/action.test.ts
+++ src/monaco/__tests__/action.test.ts
@@ -1,4 +1,6 @@
 import { CommandsRegistry } from 'monaco-editor/esm/vs/platform/commands/common/commands';
+import { MenuRegistry } from 'monaco-editor/esm/vs/platform/actions/common/actions';
+import { ContextKeyExpr } from 'monaco-editor/esm/vs/platform/contextkey/common/contextkey';
 
 import { Action2, registerAction2 } from 'mo/monaco/action';
 import { KeybindingWeight } from '../common';
@@ -15,12 +17,15 @@ describe('Test monaco action', () => {
                     label: Test.LABEL,
                     title: Test.LABEL,
                     alias: Test.LABEL,
-                    precondition: undefined,
+                    precondition: true,
                     f1: true, // Whether show the QuickOpenFile in Command Palette
                     keybinding: {
                         weight: KeybindingWeight.WorkbenchContrib,
-                        when: undefined,
-                        // eslint-disable-next-line new-cap
+                        when: ContextKeyExpr.true,
+                    },
+                    menu: {
+                        id: 'testMenu',
+                        name: 'test',
                     },
                 });
             }
@@ -32,5 +37,60 @@ describe('Test monaco action', () => {
         expect(testAction.dispose).not.toBeUndefined();
         expect(command).not.toBeUndefined();
         expect(mockRun).toBeCalled();
+        testAction.dispose();
+    });
+
+    test('Register Menus via Action2 class', () => {
+        class Test extends Action2 {
+            static readonly ID = 'TestMenu';
+            constructor() {
+                super({
+                    id: Test.ID,
+                    precondition: true,
+                    f1: false, // Whether show the QuickOpenFile in Command Palette
+                    keybinding: {
+                        weight: KeybindingWeight.WorkbenchContrib,
+                        when: ContextKeyExpr.true,
+                    },
+                    menu: [
+                        {
+                            id: 'testMenu1',
+                            name: 'test1',
+                        },
+                        {
+                            id: 'testMenu2',
+                            name: 'test2',
+                        },
+                    ],
+                });
+            }
+            run() {}
+        }
+        registerAction2(Test);
+        const menu = MenuRegistry.getMenuItems('testMenu1');
+        expect(menu.length).toBe(1);
+    });
+
+    test('Register Keybindings via Action2 class', () => {
+        class Test extends Action2 {
+            static readonly ID = 'testKeybinding';
+            constructor() {
+                super({
+                    id: Test.ID,
+                    precondition: false,
+                    f1: true, // Whether show the QuickOpenFile in Command Palette
+                    keybinding: [
+                        {
+                            weight: KeybindingWeight.WorkbenchContrib,
+                            when: ContextKeyExpr.true,
+                        },
+                    ],
+                });
+            }
+            run() {}
+        }
+        registerAction2(Test);
+        const command = CommandsRegistry.getCommand(Test.ID);
+        expect(command).not.toBeUndefined();
     });
 });

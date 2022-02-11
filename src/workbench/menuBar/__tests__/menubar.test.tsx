2294b3d2f147c2c5dea8c130ffa98162d4919f94
Shawn <1322397827@qq.com>
2022-02-11 16:39:35 +0800

    fix: the interaction bug of MenuBar in horizontal mode (#647)
    * fix: the interaction bug of MenuBar in horizontal mode
    
    * test: update unit test code

--- src/workbench/menuBar/__tests__/menubar.test.tsx
+++ src/workbench/menuBar/__tests__/menubar.test.tsx
@@ -124,7 +124,7 @@ describe('Test MenuBar Component', () => {
         expect(mockFn).toBeCalled();
     });
 
-    test('Should support to execute the handleClickMenuBar method in HorizontalView', async () => {
+    test('Should support to hide the menu by clicking the menu item at the root', async () => {
         const { getByText } = render(
             <MenuBar
                 data={menuData}
@@ -146,6 +146,35 @@ describe('Test MenuBar Component', () => {
         });
 
         fireEvent.click(elem);
+        await waitFor(() => {
+            expect(ulElem?.style.opacity).toBe('0');
+        });
+
+        document.elementsFromPoint = originalFunc;
+    });
+
+    test('Should support to hide menu by clicking the menu item of the submenu', async () => {
+        const { getByText } = render(
+            <MenuBar
+                data={menuData}
+                onClick={TEST_FN}
+                mode={MenuBarMode.horizontal}
+            />
+        );
+        const elem = getByText(TEST_ID);
+        const liElem = elem.closest('li');
+        const elemArr = liElem ? [liElem] : [];
+        const spanElem = getByText(TEST_DATA);
+        const ulElem = spanElem.closest('ul');
+        const originalFunc = document.elementsFromPoint;
+        document.elementsFromPoint = jest.fn(() => elemArr);
+
+        fireEvent.click(elem);
+        await waitFor(() => {
+            expect(ulElem?.style.opacity).toBe('1');
+        });
+
+        fireEvent.click(spanElem);
         await waitFor(() => {
             expect(ulElem?.style.opacity).toBe('0');
         });

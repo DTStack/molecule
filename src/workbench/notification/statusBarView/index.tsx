cc8ac0dd2e921fda86ed2d3f219f1ce5e1d0077a
mortalYoung <yangwei1@outlook.com>
2022-02-28 18:04:39 +0800

    refactor: improve the notifiction bell

--- src/workbench/notification/statusBarView/index.tsx
+++ src/workbench/notification/statusBarView/index.tsx
@@ -1,8 +1,15 @@
-import React, { memo } from 'react';
+import ReactDOM from 'react-dom';
+import React, { memo, useEffect, useRef } from 'react';
 import { Icon } from 'mo/components/icon';
 import type { INotification } from 'mo/model';
 import { NotificationPane } from '../notificationPane';
 import type { INotificationController } from 'mo/controller';
+import { ID_APP } from 'mo/common/id';
+import { classNames, getBEMModifier, prefixClaName } from 'mo/common/className';
+import './index.scss';
+
+const defaultBellClassName = prefixClaName('bell');
+const activeBellClassName = getBEMModifier(defaultBellClassName, 'active');
 
 export function NotificationStatusBarView(
     props: INotification & Partial<INotificationController>
@@ -16,20 +23,45 @@ export function NotificationStatusBarView(
         onActionBarClick,
         onCloseNotification,
     } = props;
+    const wrapper = useRef<HTMLDivElement>();
     const hasNotifications = data.length > 0;
     const renderIcon = hasNotifications ? 'bell-dot' : 'bell';
+
+    useEffect(() => {
+        const container = document.getElementById(ID_APP);
+        if (container) {
+            wrapper.current = wrapper.current || document.createElement('div');
+            container.appendChild(wrapper.current);
+            ReactDOM.render(
+                <NotificationPane
+                    id={id}
+                    data={data}
+                    actionBar={actionBar}
+                    showNotifications={showNotifications}
+                    onActionBarClick={onActionBarClick}
+                    onCloseNotification={onCloseNotification}
+                />,
+                wrapper.current
+            );
+        }
+    }, [
+        id,
+        data,
+        actionBar,
+        showNotifications,
+        onActionBarClick,
+        onCloseNotification,
+    ]);
+
     return (
-        <>
-            <Icon onClick={onClick} type={renderIcon} />
-            <NotificationPane
-                id={id}
-                data={data}
-                actionBar={actionBar}
-                showNotifications={showNotifications}
-                onActionBarClick={onActionBarClick}
-                onCloseNotification={onCloseNotification}
-            />
-        </>
+        <Icon
+            className={classNames(
+                defaultBellClassName,
+                showNotifications && activeBellClassName
+            )}
+            onClick={onClick}
+            type={renderIcon}
+        />
     );
 }
 export default memo(NotificationStatusBarView);

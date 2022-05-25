f5052ee3719580318d51a5df3920714f0a816a7c
mumiao <mumiao@dtstack.com>
2022-05-25 18:42:56 +0800

    fix: clear debugger info

--- src/extensions/folderTree/index.tsx
+++ src/extensions/folderTree/index.tsx
@@ -1,11 +1,3 @@
-/*
- * @Author: mumiao mumiao@dtstack.com
- * @Date: 2022-05-25 14:50:36
- * @LastEditors: mumiao mumiao@dtstack.com
- * @LastEditTime: 2022-05-25 18:40:42
- * @FilePath: /molecule/src/extensions/folderTree/index.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import molecule from 'mo';
 import { IExtension } from 'mo/model/extension';
 import {

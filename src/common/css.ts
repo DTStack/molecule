6f413e90b2828620ffa911c14dbd166142b35dd2
mortalYoung <yangwei1@outlook.com>
2022-08-25 11:22:40 +0800

    chore: remove todo comments

--- src/common/css.ts
+++ src/common/css.ts
@@ -1,7 +1,6 @@
 /**
  * px = em * parentElementFontSize
  * @param em em value
- * TODO: Use Template Literal Types replace fontSize typing
  */
 export function em2Px(em: number, fontSize: number): number {
     return em * fontSize;

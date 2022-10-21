5028a9551a3703246a65e740314464bc1fcabf02
mumiao <1270865802zl@gmail.com>
2022-10-21 12:20:11 +0800

    test: fix test cases

--- src/components/dialog/__tests__/actionbutton.test.tsx
+++ src/components/dialog/__tests__/actionbutton.test.tsx
@@ -4,8 +4,6 @@ import renderer from 'react-test-renderer';
 
 import ActionButton from '../actionButton';
 
-const TEST_ID = 'test-id';
-
 describe('Test ActionButton Component', () => {
     test('Match ActionButton Snapshot', () => {
         const TEST_FN = jest.fn();
@@ -13,39 +11,27 @@ describe('Test ActionButton Component', () => {
             <ActionButton close={TEST_FN}></ActionButton>
         );
         const tree = component.toJSON();
-
         expect(tree).toMatchSnapshot();
     });
 
     test('The ActionButton actionFn', () => {
         const ACTION_FN = jest.fn(() => false);
         const MODAL_FN = jest.fn();
-        const wrapper = render(
-            <ActionButton
-                data-testid={TEST_ID}
-                actionFn={ACTION_FN}
-                close={MODAL_FN}
-            />
+        const { container } = render(
+            <ActionButton actionFn={ACTION_FN} close={MODAL_FN} />
         );
-        const button = wrapper.getByTestId(TEST_ID);
-
-        fireEvent.click(button);
+        fireEvent.click(container.querySelector('.mo-btn')!);
         expect(ACTION_FN).toBeCalled();
         expect(MODAL_FN).toBeCalled();
     });
 
-    test('The ActionButton closeModal', () => {
+    test('The ActionButton close', () => {
         const CLOSE_FN = jest.fn();
         const ACTION_FN = jest.fn(() => Promise.resolve(1));
-        const wrapper = render(
-            <ActionButton
-                data-testid={TEST_ID}
-                actionFn={ACTION_FN}
-                close={CLOSE_FN}
-            />
+        const { container } = render(
+            <ActionButton actionFn={ACTION_FN} close={CLOSE_FN} />
         );
-        const button = wrapper.getByTestId(TEST_ID);
-
+        const button = container.querySelector('.mo-btn')!;
         fireEvent.click(button);
         expect(ACTION_FN).toBeCalled();
     });
